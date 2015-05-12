82a6b8c36f8ed350baa073f0579620e735f5ffba
Damien Mortini <ddotmnn+git@gmail.com>
2015-05-12 20:01:11 +0100

    add CustomElement class

---
+++ dom/CustomElement.js
@@ -0,0 +1,21 @@
+export default class CustomElement extends HTMLElement {
+  createdCallback() {
+    if(this.template) {
+      let templateClone = document.importNode(this.template.content, true);
+      this.createShadowRoot().appendChild(templateClone);
+    }
+  }
+  attachedCallback() {}
+  detachedCallback() {}
+  attributeChangedCallback(attr, oldVal, newVal) {}
+  static register(name, template) {
+    document.registerElement(name, {
+      prototype: Object.create(this.prototype, {
+        template: {
+          value: template,
+          enumerable: true
+        }
+      })
+    });
+  }
+}
