b26f105c4f658969399c625b6cefec48113a9609
Damien Mortini <ddotmnn+git@gmail.com>
2015-05-15 20:39:59 +0100

    clean gl calls

--- webgl/GLSLView.js
+++ webgl/GLSLView.js
@@ -1,46 +1,45 @@
 export default class GLSLView {
   constructor (canvas, fragmentShaderStr) {
 
-    let gl = this.gl = canvas.getContext('webgl');
+    this.gl = canvas.getContext('webgl');
 
-    let vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader, `
+    let vertexShader = this.gl.createShader(this.gl.VERTEX_SHADER);
+    this.gl.shaderSource(vertexShader, `
       attribute vec2 aPosition;
       void main() {
           gl_Position = vec4(aPosition, 0, 1);
       }
     `);
-    gl.compileShader(vertexShader);
-    console.log( gl.getShaderInfoLog(vertexShader) );
+    this.gl.compileShader(vertexShader);
+    console.log( this.gl.getShaderInfoLog(vertexShader) );
 
-    let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader, fragmentShaderStr);
-    gl.compileShader(fragmentShader);
-    console.log( gl.getShaderInfoLog(fragmentShader) );
+    let fragmentShader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
+    this.gl.shaderSource(fragmentShader, fragmentShaderStr);
+    this.gl.compileShader(fragmentShader);
+    console.log( this.gl.getShaderInfoLog(fragmentShader) );
 
-    this.program = gl.createProgram();
-    gl.attachShader(this.program, vertexShader);
-    gl.attachShader(this.program, fragmentShader);
-    gl.linkProgram(this.program);
-    gl.useProgram(this.program);
+    this.program = this.gl.createProgram();
+    this.gl.attachShader(this.program, vertexShader);
+    this.gl.attachShader(this.program, fragmentShader);
+    this.gl.linkProgram(this.program);
+    this.gl.useProgram(this.program);
 
-    let buffer = gl.createBuffer();
+    let buffer = this.gl.createBuffer();
     let bufferData = [
         -1, -1,
         -1, 1,
         1, -1,
         1, 1
     ];
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(bufferData), gl.STATIC_DRAW);
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(bufferData), this.gl.STATIC_DRAW);
 
-    let aPosition = gl.getAttribLocation(this.program, 'aPosition');
-    gl.enableVertexAttribArray(aPosition);
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    let aPosition = this.gl.getAttribLocation(this.program, 'aPosition');
+    this.gl.enableVertexAttribArray(aPosition);
+    this.gl.vertexAttribPointer(aPosition, 2, this.gl.FLOAT, false, 0, 0);
   }
 
   update () {
-    let gl = this.gl;
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
   }
 }
