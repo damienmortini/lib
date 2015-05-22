d83d845b0fab1a944c1a09a0b5505856559b7433
Damien Mortini <ddotmnn+git@gmail.com>
2015-05-22 15:41:55 +0100

    add copy to polygon

--- math/Polygon.js
+++ math/Polygon.js
@@ -1,5 +1,15 @@
+import Vector2 from "./Vector2";
+
 export default class Polygon {
-  constructor(vertices) {
+  constructor(vertices = []) {
     this.vertices = vertices;
+  }
+
+  copy(polygon) {
+    this.vertices = [];
+    for(let vertex of polygon.vertices) {
+      this.vertices.push(new Vector2().copy(vertex));
+    }
+    return this;
   }
 }
