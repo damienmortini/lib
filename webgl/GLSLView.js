1201f4ecdb94b2b847d696cd0c45cfe55de5a94f
Damien Mortini <ddotmnn+git@gmail.com>
2015-08-17 23:37:29 +0100

    fix viewport size issue on GLSLView

--- webgl/GLSLView.js
+++ webgl/GLSLView.js
@@ -1,7 +1,7 @@
 export default class GLSLView {
   constructor (canvas, fragmentShaderStr) {
     this.canvas = canvas;
-    this.gl = this.canvas.getContext('webgl');
+    this.gl = this.canvas.getContext("webgl");
 
     let vertexShader = this.gl.createShader(this.gl.VERTEX_SHADER);
     this.gl.shaderSource(vertexShader, `
@@ -11,12 +11,16 @@ export default class GLSLView {
       }
     `);
     this.gl.compileShader(vertexShader);
-    console.log( this.gl.getShaderInfoLog(vertexShader) );
+    if(!this.gl.getShaderParameter(vertexShader, this.gl.COMPILE_STATUS)) {
+      console.error("Error compiling shader:", this.gl.getShaderInfoLog(vertexShader));
+    }
 
     let fragmentShader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
     this.gl.shaderSource(fragmentShader, fragmentShaderStr);
     this.gl.compileShader(fragmentShader);
-    console.log( this.gl.getShaderInfoLog(fragmentShader) );
+    if(!this.gl.getShaderParameter(fragmentShader, this.gl.COMPILE_STATUS)) {
+      console.error("Error compiling shader:", this.gl.getShaderInfoLog(fragmentShader));
+    }
 
     this.program = this.gl.createProgram();
     this.gl.attachShader(this.program, vertexShader);
@@ -34,12 +38,15 @@ export default class GLSLView {
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
     this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(bufferData), this.gl.STATIC_DRAW);
 
-    let aPosition = this.gl.getAttribLocation(this.program, 'aPosition');
+    let aPosition = this.gl.getAttribLocation(this.program, "aPosition");
     this.gl.enableVertexAttribArray(aPosition);
     this.gl.vertexAttribPointer(aPosition, 2, this.gl.FLOAT, false, 0, 0);
   }
 
   update () {
+    this.gl.viewport(0, 0, this.gl.drawingBufferWidth, this.gl.drawingBufferHeight);
+    this.gl.clearColor(0, 0, 0, 1);
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
     this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
   }
 }
