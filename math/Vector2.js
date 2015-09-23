300be8e5ccc7bc394d47643f97a516870e984e40
Damien Mortini <ddotmnn+git@gmail.com>
2015-09-23 19:12:31 +0100

    add components array to Vector2 => TODO use gl-matrix mehods instead of current ones

--- math/Vector2.js
+++ math/Vector2.js
@@ -1,8 +1,27 @@
+import Vec2 from "../node_modules/gl-matrix/src/gl-matrix/vec2.js";
+
 export default class Vector2 {
   constructor(x = 0, y = 0) {
+    this.components = Vec2.create();
     this.x = x;
     this.y = y;
     return this;
+  }
+
+  get x() {
+    return this.components[0];
+  }
+
+  set x(value) {
+    this.components[0] = value;
+  }
+
+  get y() {
+    return this.components[1];
+  }
+
+  set y(value) {
+    this.components[1] = value;
   }
 
   get length () {
