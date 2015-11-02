ec2fe6c35409e21bb406ff3179210126cfb1df88
Damien Mortini <damien@Damiens-MacBook-Pro.local>
2015-11-02 11:14:08 +0000

    add timestamp

--- dom/LoopElement.js
+++ dom/LoopElement.js
@@ -1,15 +1,22 @@
 import CustomElement from "./CustomElement.js";
 
 export default class LoopElement extends CustomElement {
-  createdCallback() {
+  createdCallback({autostart = true} = {}) {
     super.createdCallback();
     this._updateBinded = this.update.bind(this);
+
+    this.autostart = autostart;
+
+    this._previousTimestamp = 0;
+    this.deltaTime = 0;
   }
 
   attachedCallback() {
     window.addEventListener("blur", this._stopBinded = this.stop.bind(this));
     window.addEventListener("focus", this._startBinded = this.start.bind(this));
-    this.start();
+    if(this.autostart) {
+      this.start();
+    }
   }
 
   detachedCallback() {
@@ -20,6 +27,7 @@ export default class LoopElement extends CustomElement {
 
   start() {
     this.stop();
+    this._previousTimestamp = window.performance ? window.performance.now() : Date.now();
     this.update();
   }
 
@@ -29,5 +37,9 @@ export default class LoopElement extends CustomElement {
 
   update() {
     this._requestAnimationFrameId = requestAnimationFrame(this._updateBinded);
+
+    let timestamp = window.performance ? window.performance.now() : Date.now();
+    this.deltaTime = timestamp - this._previousTimestamp;
+    this._previousTimestamp = timestamp;
   }
 }
