477de2df4b4992dde7d2d8d85f4015913de0def9
Damien Mortini <damien@Damiens-MBP.localdomain>
2015-11-12 18:15:29 +0100

    add mute methods to soundManager

--- audio/SoundManager.js
+++ audio/SoundManager.js
@@ -2,25 +2,47 @@ import {Howler, Howl} from "howler";
 
 let soundMap = new Map();
 
-if(/mute/.test(window.location.search)) {
-  Howler.mute();
-}
+let muteLooped = /\bmutelooped\b/.test(window.location.search);
+
+let muted = false;
 
 export default class SoundManager {
-  static add(url, options) {
+  static add(url, {loop = false} = {}) {
     let sound = new Howl({
-      urls: [url]
+      urls: [url],
+      loop: loop
     });
-    Object.assign(sound, options);
     let split = url.split("/");
     let name = split[split.length - 1].split(".")[0];
     soundMap.set(name, sound);
-    return sound;
   }
-  static play(name, options) {
+  static play(name, {loop = undefined} = {}) {
     let sound = soundMap.get(name);
-    Object.assign(sound, options);
+    if(loop !== undefined) {
+      sound.loop(loop);
+    }
+    if(sound.loop() && muteLooped) {
+      sound.mute();
+    }
     sound.play();
-    return sound;
   }
+  static toggleMute() {
+    if(muted) {
+      SoundManager.unmute();
+    } else {
+      SoundManager.mute();
+    }
+  }
+  static mute() {
+    muted = true;
+    Howler.mute();
+  }
+  static unmute() {
+    muted = false;
+    Howler.unmute();
+  }
+}
+
+if(/\bmute\b/.test(window.location.search)) {
+  SoundManager.mute();
 }
