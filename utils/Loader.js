0f2554654dc5e55a69e59c22395dd46cbb87e391
Damien Mortini <damien@Damiens-MBP.localdomain>
2015-11-12 18:15:06 +0100

    add loadImages

--- utils/Loader.js
+++ utils/Loader.js
@@ -1,7 +1,20 @@
+let files = new Map();
+
 export default class Loader {
+  static loadImages(urls) {
+    let promises = [];
+    for(let url of urls) {
+      promises.push(Loader.loadImage(url));
+    }
+    return Promise.all(promises);
+  }
   static loadImage(url) {
+    let image = files.get(url);
+    if(image) {
+      return Promise.resolve(image);
+    }
     return new Promise(function(resolve, reject) {
-      let image = new Image();
+      image = new Image();
       image.onload = () => {
         resolve(image);
       };
