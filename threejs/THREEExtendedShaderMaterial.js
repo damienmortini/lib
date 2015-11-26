fbfae174bf5572612ca2266ebbcc60177012c4ab
Damien Mortini <damien@Damiens-MacBook-Pro.local>
2015-11-26 18:25:10 +0000

    clean THREEExtendedShaderMaterial

--- threejs/THREEExtendedShaderMaterial.js
+++ threejs/THREEExtendedShaderMaterial.js
@@ -3,25 +3,26 @@ import THREE from "THREE";
 import THREEShader from "dlib/threejs/THREEShader.js";
 
 export default class THREEExtendedShaderMaterial extends THREE.ShaderMaterial {
-  constructor (originalShaderName, vertexShaderChunks = ["", ""], fragmentShaderChunks = ["", ""]) {
+  constructor (originalShaderName, {vertexShaderChunks = {}, fragmentShaderChunks = {}} = {}) {
+
+    vertexShaderChunks = Object.assign({prefix: "", main: "", suffix: ""}, vertexShaderChunks);
+    fragmentShaderChunks = Object.assign({prefix: "", main: "", suffix: ""}, fragmentShaderChunks);
+
     let originalShader = THREE.ShaderLib[originalShaderName];
-    let tempShader = new THREEShader(vertexShaderChunks[0] + vertexShaderChunks[1], fragmentShaderChunks[0] + fragmentShaderChunks[1]);
+    let tempShader = new THREEShader(vertexShaderChunks.prefix, fragmentShaderChunks.prefix);
+
+    var regExp = /([\s\S]*?\bvoid\b +\bmain\b[\s\S]*?{)([\s\S]*)}/m;
+
+    let generateSubstringFromChunks = (chunks) => {
+      return `${chunks.prefix}\n\n$1\n\n${chunks.main}\n\n$2\n\n${chunks.suffix}\n\n}`
+    }
 
     super({
       uniforms: Object.assign(originalShader.uniforms, tempShader.uniforms),
-      vertexShader: `${vertexShaderChunks[0]}
-        ${originalShader.vertexShader.replace("void main() {", `
-          void main() {
-            ${vertexShaderChunks[1]}
-        `)}
-      `,
-      fragmentShader: `
-        ${fragmentShaderChunks[0]}
-        ${originalShader.fragmentShader.slice(0, originalShader.fragmentShader.lastIndexOf("}"))}
-        ${fragmentShaderChunks[1]}
-        }
-      `
+      vertexShader: originalShader.vertexShader.replace(regExp, generateSubstringFromChunks(vertexShaderChunks)),
+      fragmentShader: originalShader.fragmentShader.replace(regExp, generateSubstringFromChunks(fragmentShaderChunks))
     });
+
     this.lights = /lambert|phong/.test(originalShaderName);
   }
 }
