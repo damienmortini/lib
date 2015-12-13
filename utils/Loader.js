858cb16408ad5fd7896119837d6cd794074a0a9d
Damien Mortini <damien@Damiens-MacBook-Pro.local>
2015-12-13 19:00:56 +0000

    add crossorigin option to Loader

--- utils/Loader.js
+++ utils/Loader.js
@@ -1,20 +1,21 @@
 let files = new Map();
 
 export default class Loader {
-  static loadImages(urls) {
+  static loadImages(urls, options) {
     let promises = [];
     for(let url of urls) {
-      promises.push(Loader.loadImage(url));
+      promises.push(Loader.loadImage(url, options));
     }
     return Promise.all(promises);
   }
-  static loadImage(url) {
+  static loadImage(url, {crossOrigin = null} = {}) {
     let image = files.get(url);
     if(image) {
       return Promise.resolve(image);
     }
     return new Promise(function(resolve, reject) {
       image = new Image();
+      image.crossOrigin = crossOrigin;
       image.onload = () => {
         resolve(image);
       };
