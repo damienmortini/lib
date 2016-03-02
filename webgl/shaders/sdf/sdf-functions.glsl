479a9d1705cac453d4750366cf3f6f69ba6226d7
Damien Mortini <damien@Damiens-MacBook-Pro.local>
2016-03-02 19:39:28 +0000

    divide sdf functions

--- webgl/shaders/sdf/sdf-functions.glsl
+++ webgl/shaders/sdf/sdf-functions.glsl
@@ -1,23 +1,3 @@
-struct Camera
-{
-  float near;
-  float far;
-  float fov;
-  float aspect;
-  mat4 matrixWorldInverse;
-};
-
-struct Voxel
-{
-  float dist;
-  vec4 color;
-};
-
-struct Ray
-{
-  vec3 origin;
-  vec3 direction;
-};
 
 Ray rayFromCamera(vec2 uv, Camera camera) {
   float fovScaleY = tan(camera.fov * .5);
@@ -27,8 +7,8 @@ Ray rayFromCamera(vec2 uv, Camera camera) {
   vec3 rayOrigin = -(camera.modelViewMatrix[3].xyz) * mat3(camera.modelViewMatrix);
   vec3 rayDirection = normalize(vec3(uv.x * fovScaleY * camera.aspect, uv.y * fovScaleY, -1.0) * mat3(camera.modelViewMatrix));
 
-  vec3 rayOrigin = -(camera.matrixWorldInverse[3].xyz) * mat3(camera.matrixWorldInverse);
-  vec3 rayDirection = camera.matrixWorldInverse * camera.projectionMatrix * normalize(vec3(position, .5));
+  // vec3 rayOrigin = -(camera.matrixWorldInverse[3].xyz) * mat3(camera.matrixWorldInverse);
+  // vec3 rayDirection = camera.matrixWorldInverse * camera.projectionMatrix * normalize(vec3(position, .5));
 
   return Ray(rayOrigin, rayDirection);
 }
@@ -58,15 +38,59 @@ Voxel rayMarch(vec3 rayOrigin, vec3 rayDirection, float near, float far)
   }
 
   // vec3 normal = calcNormal(rayOrigin + rayDirection * dist);
-  // voxel.color *= 1. + dot(normal, normalize(vec3(cos(uTime * 100.), cos(uTime * 100.), sin(uTime * 100.))));
+  // voxel.material *= 1. + dot(normal, normalize(vec3(cos(uTime * 100.), cos(uTime * 100.), sin(uTime * 100.))));
 
   return voxel;
 }
 
 
-Voxel rayMarchFromCamera(Camera camera) {
-  Ray ray = rayFromCamera(camera);
-  Voxel voxel = rayMarch(ray.origin, ray.direction);
+Voxel rayMarchFromCamera(vec2 uv, Camera camera) {
+  Ray ray = rayFromCamera(uv, camera);
+  Voxel voxel = rayMarch(ray.origin, ray.direction, uCamera.near, uCamera.far);
+  return voxel;
+}
+
+Voxel rayMarchTerrain(vec3 rayOrigin, vec3 rayDirection)
+{
+  Voxel voxel = Voxel(uCamera.far, vec4(0.0));
+
+  float lastHeight = 0.;
+  float lastY = 0.;
+
+  float dist = 0.;
+
+  float steps = 200.;
+  float step = pow(steps, 1./steps);
+  // const float step = 1.0789723114;
+
+  float distToPlane = -dot(rayOrigin, vec3(0., 1., 0.)) / dot(rayDirection, vec3(0., 1., 0.));
+
+
+  for(float i = 0.; i < 200.; i++) {
+    dist = distToPlane - steps + i;
+    // dist = pow(step, i);
+    vec3 p = rayOrigin + rayDirection * dist;
+    float height = height(p);
+
+    if(p.y < height) {
+        // interpolate the intersection distance
+        // resT = t - dt + dt * (lastHeight - lastY) / (p.y - lastY - h + lastHeight);
+        voxel.material = vec4(vec3(p.y / scale), 1.0);
+        break;
+    }
+
+    // step = 0.01 * dist;
+    lastHeight = height;
+    lastY = p.y;
+
+    // rayMarchingStep = voxel.dist;
+    // dist += rayMarchingStep;
+    // voxel.dist = dist;
+  }
+
+  // vec3 normal = calcNormal(rayOrigin + rayDirection * dist);
+  // voxel.material *= 1. + dot(normal, normalize(vec3(cos(uTime * 100.), cos(uTime * 100.), sin(uTime * 100.))));
+
   return voxel;
 }
 
@@ -85,6 +109,6 @@ Voxel rayMarchFromCamera(Camera camera) {
 //
 //   Voxel voxel = rayMarch(rayOrigin, rayDirection);
 //
-//   gl_FragColor = vec4(voxel.color.rgb, 1.);
+//   gl_FragColor = vec4(voxel.material.rgb, 1.);
 //   // gl_FragColor = texture2D(uTexture, position);
 // }
