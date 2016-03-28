d3124174dcc4e93ccbe67f6f4f6033846077a0e6
Damien Mortini <damien@Damiens-MacBook-Pro.local>
2016-03-28 18:59:35 +0100

    add OBJ log export and get positions to Facetracker

--- track/FaceTracker.js
+++ track/FaceTracker.js
@@ -1,9 +1,23 @@
 export default class FaceTracker {
   constructor(input) {
     this.tracker = new clm.tracker();
-    console.log(pModel);
     this.tracker.init(pModel);
-    // this.tracker.start(input);
+    this.tracker.start(input);
+  }
+
+  logFaceOBJ() {
+    let str = "";
+    for (let vertex of pModel.shapeModel.meanShape) {
+      str += `v ${vertex[0]} ${vertex[1]} 0\n`
+    }
+    for (let indice of pModel.path.vertices.concat([[44, 61, 56],[61, 60, 56],[56, 60, 57],[60, 59, 57],[57, 59, 58],[58, 59, 50] ])) {
+      str += `f ${++indice[0]} ${++indice[1]} ${++indice[2]}\n`
+    }
+    console.log(str);
+  }
+
+  get positions() {
+    return this.tracker.getCurrentPosition();
   }
 
   draw(context) {
