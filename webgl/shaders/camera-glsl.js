e1a608658ccc6889efc5960903b5f9fe1395a734
d̶m̶m̶n̶ <damien@Damiens-MacBook-Pro.local>
2016-05-11 00:25:18 +0100

    update glsl-camera

--- webgl/shaders/camera-glsl.js
+++ webgl/shaders/camera-glsl.js
@@ -6,19 +6,15 @@ struct Camera
   float far;
   float fov;
   float aspect;
-  mat4 worldInverseMatrix;
+  mat4 inverseMatrix;
   mat4 projectionMatrix;
 };
 
-Ray rayFromCamera(vec2 uv, Camera camera) {
-  vec2 position = uv * 2. - 1.;
-
+Ray rayFromCamera(vec2 position, Camera camera) {
   float fovScaleY = tan(camera.fov * .5);
 
-  vec3 vCameraForward = vec3(0.0, 0.0, -1.0) * mat3(camera.worldInverseMatrix);
-
-  vec3 rayOrigin = -(camera.worldInverseMatrix[3].xyz) * mat3(camera.worldInverseMatrix);
-  vec3 rayDirection = normalize(vec3(position.x * fovScaleY * camera.aspect, position.y * fovScaleY, -1.0) * mat3(camera.worldInverseMatrix));
+  vec3 rayOrigin = -camera.inverseMatrix[3].xyz * mat3(camera.inverseMatrix);
+  vec3 rayDirection = normalize(vec3(position.x * fovScaleY * camera.aspect, position.y * fovScaleY, -1.0) * mat3(camera.inverseMatrix));
 
   return Ray(rayOrigin, rayDirection);
 }
