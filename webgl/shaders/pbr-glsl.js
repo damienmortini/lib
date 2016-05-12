1d32b8600bb47a39d347b95cb54a77ed3bf77154
d̶m̶m̶n̶ <damien@Damiens-MacBook-Pro.local>
2016-05-12 19:27:33 +0100

    generate blur texture for pbr

--- webgl/shaders/pbr-glsl.js
+++ webgl/shaders/pbr-glsl.js
@@ -1,6 +1,12 @@
 // from https://www.shadertoy.com/view/XsfXWX
 
-export default function() {
+export default function({
+  map = `
+  vec3 metalRayPick(vec3 p) {
+    return vec3(0.);
+  }
+  `
+} = {}) {
   return `
 struct Light
 {
@@ -44,31 +50,35 @@ float GGX(vec3 N, vec3 V, vec3 L, float roughness, float F0) {
 vec3 computePBRLighting (
   Light light,
   Ray ray,
+  vec3 position,
   vec3 normal,
   vec3 albedo,
   float metalness,
   float roughness,
-  samplerCube texture,
-  samplerCube textureBlured
+  samplerCube reflectionTexture
 ) {
 
   // material
   float fresnel_pow = 1.5;
   vec3 color_mod = vec3(1.0);
-  light.color *= textureCube(texture, vec3(1.0,0.0,0.0)).xyz * 1.2;
+  light.color *= textureCube(reflectionTexture, vec3(1.0,0.0,0.0)).xyz * 1.2;
 
   // IBL
-  vec3 ibl_diffuse = textureCube(textureBlured, normal).xyz;
-  vec3 ibl_reflection = textureCube(textureBlured, reflect(ray.direction, normal)).xyz;
+  vec3 ibl_diffuse = textureCube(reflectionTexture, normalize(normal * vec3(rand(position.x * 2. - 1.), rand(position.y * 2. - 1.), rand(position.z * 2. - 1.)) * .5)).xyz;
+  float blurRatio = (ibl_diffuse.r + ibl_diffuse.g + ibl_diffuse.b) / 3.;
+  ibl_diffuse = ibl_diffuse * blurRatio + (1. - blurRatio);
+  vec3 ibl_reflection = textureCube(reflectionTexture, normalize(reflect(ray.direction, normal) + vec3(rand(position.x * 2. - 1.), rand(position.y * 2. - 1.), rand(position.z * 2. - 1.)) * .5)).xyz;
+  blurRatio = (ibl_reflection.r + ibl_reflection.g + ibl_reflection.b) / 3.;
+  ibl_reflection = ibl_reflection * blurRatio + (1. - blurRatio);
 
   // fresnel
   float fresnel = max(1.0 - dot(normal, -ray.direction), 0.0);
   fresnel = pow(fresnel, fresnel_pow);
 
   // reflection
-  vec3 refl = textureCube(texture, reflect(ray.direction,normal)).xyz;
-  refl = mix(refl,ibl_reflection,(1.0-fresnel)*roughness);
-  refl = mix(refl,ibl_reflection,roughness);
+  vec3 refl = textureCube(reflectionTexture, reflect(ray.direction,normal)).xyz;
+  refl = mix(refl, ibl_reflection, (1.0-fresnel)*roughness);
+  refl = mix(refl, ibl_reflection, roughness);
 
   // specular
   float power = 1.0 / max(roughness * 0.4,0.01);
