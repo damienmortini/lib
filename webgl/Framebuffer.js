7e71b0b5b6b7db3d5e5e19d35387ace4e5178e09
d̶m̶m̶n̶ <damien@Damiens-MacBook-Pro.local>
2016-05-12 13:17:13 +0100

    update framebuffer to live on its own

--- webgl/Framebuffer.js
+++ webgl/Framebuffer.js
@@ -1,3 +1,43 @@
-import Framebuffer from "gl-fbo";
+import createFBO from "gl-fbo";
 
-export default Framebuffer;
+export default class Framebuffer {
+  constructor(gl, width, height, options) {
+    this.stackglFramebuffer = createFBO(gl, [width, height], options);
+  }
+
+  get width() {
+    return this.stackglFramebuffer.shape[0];
+  }
+
+  set width(value) {
+    this.stackglFramebuffer.shape = [value, this.height];
+  }
+
+  get height() {
+    return this.stackglFramebuffer.shape[1];
+  }
+
+  set height(value) {
+    this.stackglFramebuffer.shape = [this.width, value];
+  }
+
+  get colors() {
+    return this.stackglFramebuffer.color;
+  }
+
+  get depth() {
+    return this.stackglFramebuffer.depth;
+  }
+
+  get webGLFramebuffer() {
+    return this.stackglFramebuffer.handle;
+  }
+
+  bind() {
+    this.stackglFramebuffer.bind();
+  }
+
+  dispose() {
+    this.stackglFramebuffer.dispose();
+  }
+};
