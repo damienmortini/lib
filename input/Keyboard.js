e9b1092770aa481037dcfd8b43cd0a736960b3ec
dmmn <ddotmnn+git@gmail.com>
2016-07-07 15:50:05 +0100

    add signals to keyboard

--- input/Keyboard.js
+++ input/Keyboard.js
@@ -1,15 +1,45 @@
+import Signal from "../utils/Signal.js";
+
 let keysDown = new Set();
 
+let onKeyDown = new Signal();
+let onKeyUp = new Signal();
+
 export default class Keyboard {
+  static get LEFT() {
+    return 37;
+  }
+  static get RIGHT() {
+    return 39;
+  }
+  static get UP() {
+    return 38;
+  }
+  static get DOWN() {
+    return 40;
+  }
+  static get SPACE() {
+    return 32;
+  }
+  static get onKeyDown() {
+    return onKeyDown;
+  }
+  static get onKeyUp() {
+    return onKeyUp;
+  }
   static isKeyDown(keyCode) {
     return keysDown.has(keyCode);
   }
 }
 
 window.addEventListener("keydown", (e) => {
+  if(!Keyboard.isKeyDown(e.keyCode)) {
+    onKeyDown.dispatch(e.keyCode);
+  }
   keysDown.add(e.keyCode);
 });
 
 window.addEventListener("keyup", (e) => {
   keysDown.delete(e.keyCode);
+  onKeyUp.dispatch(e.keyCode);
 });
