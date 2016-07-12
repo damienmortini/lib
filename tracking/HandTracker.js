36b2823d1aa8bffefb725ba1555bba22d59630e9
dmmn <ddotmnn+git@gmail.com>
2016-07-12 13:41:05 +0100

    add type/id and remove signal

--- tracking/HandTracker.js
+++ tracking/HandTracker.js
@@ -8,7 +8,10 @@ import Vector3 from "../math/Vector3.js";
 import Quaternion from "../math/Quaternion.js";
 
 class Hand {
-  constructor() {
+  constructor(handData) {
+    this.type = handData.type;
+    this.id = handData.id;
+
     this._bones = new Array(5).fill().map(value => new Array(3).fill().map(value => new Quaternion()));
 
     this._position = new Vector3();
@@ -36,7 +39,7 @@ class Hand {
   }
 
   update(handData, pointablesData) {
-    this.position.set(handData.palmPosition[0], handData.palmPosition[1], handData.palmPosition[2]);
+    this.position.set(handData.stabilizedPalmPosition[0], handData.stabilizedPalmPosition[1], handData.stabilizedPalmPosition[2]);
 
     this._vector3A.copy(handData.direction);
     this._vector3A.negate();
@@ -60,7 +63,11 @@ class Hand {
 
         let basis = pointableData.bones[i + 1].basis;
 
-        this._matrix3.fromBasis(basis[0], basis[1], basis[2]);
+        this._vector3A.copy(basis[0]);
+        if(this.type === "left") {
+          this._vector3A.negate();
+        }
+        this._matrix3.fromBasis(this._vector3A, basis[1], basis[2]);
 
         bone.fromMatrix3(this._matrix3);
 
@@ -80,15 +87,20 @@ class HandTracker {
         host: "127.0.0.1"
       });
 
-      this.hands = new Map();
+      this._hands = new Map();
 
       this.frame = null;
 
       this.onHandAdd = new Signal();
+      this.onHandRemove = new Signal();
 
       controller.on("frame", this.onFrame.bind(this));
 
       controller.connect();
+  }
+
+  get hands() {
+    return this._hands;
   }
 
   onFrame(frame) {
@@ -99,15 +111,58 @@ class HandTracker {
     }
 
     for(let handData of this.frame.hands) {
-      let hand = this.hands.get(handData.id);
+      let hand = this._hands.get(handData.id);
       if(!hand) {
-        hand = new Hand();
-        this.hands.set(handData.id, hand);
+        hand = new Hand(handData);
+        this._hands.set(handData.id, hand);
         this.onHandAdd.dispatch(hand);
       }
       hand.update(handData, frame.pointables);
+    }
+
+    for (let hand of this.hands.values()) {
+      let remove = true;
+      for (let handData of this.frame.hands) {
+        if(hand.id === handData.id) {
+          remove = false;
+        }
+      }
+      if(remove) {
+        this._hands.delete(hand.id);
+        this.onHandRemove.dispatch(hand);
+      }
     }
   }
 }
 
 export default new HandTracker();
+
+
+// this.fingerBonesHelper = [];
+// for (let i = 0; i < 5; i++) {
+//   let bones = [];
+//   let bonesGlobal = [];
+//
+//   let currentBone;
+//
+//   for (let j = 0; j < 3; j++) {
+//
+//     let bone = new THREE.Mesh(new THREE.BoxGeometry(.1, .1, .3), new THREE.MeshNormalMaterial());
+//     bone.geometry.translate(0, 0, -.15);
+//
+//     if(currentBone) {
+//       currentBone.add(bone);
+//       bone.position.z = -.3;
+//     } else {
+//       // this.add(bone);
+//       bone.position.y = .1;
+//       bone.position.z = -.2;
+//       bone.position.x = i * .3 - .75;
+//     }
+//
+//     currentBone = bone;
+//     bones[j] = bone;
+//   }
+//
+//   this.fingerBonesHelper[i] = bones;
+// }
