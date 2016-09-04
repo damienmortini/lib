73ab25ed7c3ec4c4228babff2fd5176d9e9dc69b
dmmn <ddotmnn+git@gmail.com>
2016-09-04 11:17:24 +0100

    add background param to LoopElement

--- dom/LoopElement.js
+++ dom/LoopElement.js
@@ -3,14 +3,17 @@ import Ticker from "../utils/Ticker.js";
 import CustomElement from "./CustomElement.js";
 
 export default class LoopElement extends CustomElement {
-  createdCallback({autoplay = true} = {}) {
+  createdCallback({autoplay = true, background = false} = {}) {
     super.createdCallback();
     this._autoplay = autoplay;
+    this._background = background;
   }
 
   attachedCallback() {
-    window.addEventListener("blur", this._pauseBinded = this.pause.bind(this));
-    window.addEventListener("focus", this._playBinded = this.play.bind(this));
+    if(!this._background) {
+      window.addEventListener("blur", this._pauseBinded = this.pause.bind(this));
+      window.addEventListener("focus", this._playBinded = this.play.bind(this));
+    }
     if(this._autoplay) {
       this.play();
     }
