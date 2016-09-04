73e74c677991b867c9a3d568cc608877fa887755
dmmn <ddotmnn+git@gmail.com>
2016-09-04 01:56:24 +0100

    revert options to be able to set any material params

--- three/THREEExtendedShaderMaterial.js
+++ three/THREEExtendedShaderMaterial.js
@@ -1,27 +1,33 @@
 import THREE from "three";
 
+import { ShaderMaterial, ShaderLib, UniformsUtils } from "three";
+
 import THREEShader from "./THREEShader.js";
 
-export default class THREEExtendedShaderMaterial extends THREE.ShaderMaterial {
-  constructor ({originalShaderName = "", vertexShaderHooks, fragmentShaderHooks, uniforms = {}} = {}) {
-    vertexShaderHooks = Object.assign({prefix: "", main: "", suffix: ""}, vertexShaderHooks);
-    fragmentShaderHooks = Object.assign({prefix: "", main: "", suffix: ""}, fragmentShaderHooks);
+export default class THREEExtendedShaderMaterial extends ShaderMaterial {
+  constructor (options = {}) {
+    let originalShaderName = options.originalShaderName || "basic";
+    delete options.originalShaderName;
+    let vertexShaderHooks = Object.assign({prefix: "", main: "", suffix: ""}, options.vertexShaderHooks);
+    delete options.vertexShaderHooks;
+    let fragmentShaderHooks = Object.assign({prefix: "", main: "", suffix: ""}, options.fragmentShaderHooks);
+    delete options.fragmentShaderHooks;
 
-    let originalShader = THREE.ShaderLib[originalShaderName] || {};
+    let originalShader = ShaderLib[originalShaderName] || {};
     let tempShader = new THREEShader({vertexShader: vertexShaderHooks.prefix, fragmentShader: fragmentShaderHooks.prefix});
 
-    uniforms = Object.assign(THREE.UniformsUtils.clone(originalShader.uniforms), tempShader.uniforms, uniforms);
+    options.uniforms = Object.assign(UniformsUtils.clone(originalShader.uniforms), tempShader.uniforms, options.uniforms);
 
     var regExp = /([\s\S]*?\bvoid\b +\bmain\b[\s\S]*?{)([\s\S]*)}/m;
 
     let generateSubstringFromHooks = (hooks) => {
-      return `${hooks.prefix}\n\n$1\n\n${hooks.main}\n\n$2\n\n${hooks.suffix}\n\n}`
+      return `${hooks.prefix}\n\n$1\n\n${hooks.main}\n\n$2\n\n${hooks.suffix}\n\n}`;
     }
 
     super(Object.assign({
       vertexShader: originalShader.vertexShader.replace(regExp, generateSubstringFromHooks(vertexShaderHooks)),
       fragmentShader: originalShader.fragmentShader.replace(regExp, generateSubstringFromHooks(fragmentShaderHooks))
-    }, {}));
+    }, options));
 
     for (let key of ["bumpMap", "displacementMap", "emissiveMap", "envMap", "lightMap", "map", "metalnessMap", "normalMap", "roughnessMap", "specularMap"]) {
       if(this.uniforms[key]) {
