4f826848fa19fccc3f07fca6cdc14a64c0a12e8d
dmmn <ddotmnn+git@gmail.com>
2016-09-05 12:45:58 +0100

    fix THREETrackballController

--- three/THREETrackballController.js
+++ three/THREETrackballController.js
@@ -1,19 +1,24 @@
 import { Object3D } from "three/src/core/Object3D.js";
+import { Matrix4 as THREEMatrix4 } from "three/src/math/Matrix4.js";
 
 import TrackballController from "../3d/controllers/TrackballController.js";
 import Matrix4 from "../math/Matrix4.js";
 
 export default class THREETrackballController extends TrackballController {
   constructor(object3D = new Object3D(), {domElement, distance, distanceStep} = {}) {
-    object3D.matrixAutoUpdate = false;
     object3D.updateMatrix();
     super(new Matrix4(object3D.matrix.elements), {domElement, distance, distanceStep});
+    this._matrix4 = new THREEMatrix4();
     this.object3D = object3D;
   }
 
   update() {
+    this.matrix.x = this.object3D.position.x;
+    this.matrix.y = this.object3D.position.y;
+    this.matrix.z = this.object3D.position.z;
     super.update();
-    this.object3D.matrix.fromArray(this.matrix);
-    this.object3D.matrixWorldNeedsUpdate = true;
+    this._matrix4.fromArray(this.matrix);
+    this.object3D.matrix.identity();
+    this.object3D.applyMatrix(this._matrix4);
   }
 }
