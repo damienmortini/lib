8cd667e95939f8619c4af8d3a5aba5a859e5aeb2
dmmn <ddotmnn+git@gmail.com>
2016-09-09 17:45:44 +0100

    required components test

--- ecs/Component.js
+++ ecs/Component.js
@@ -1,5 +1,11 @@
 export default class Component {
-  constructor(entity) {
+  constructor(entity, {require = []} = {}) {
     this.entity = entity;
+
+    for (let required of require) {
+      if(!this.entity.hasComponent(required)) {
+        console.error(this.entity + " needs component " + required);
+      }
+    }
   }
 }
