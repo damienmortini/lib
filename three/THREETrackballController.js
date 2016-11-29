cfa43063d8e0946e4215aa3f9193089e59f41094
dmmn <ddotmnn+git@gmail.com>
2016-11-29 18:50:44 +0000

    THREE trackball fixes

--- three/THREETrackballController.js
+++ three/THREETrackballController.js
@@ -10,9 +10,14 @@ export default class THREETrackballController extends TrackballController {
     super(new Matrix4(object3D.matrix.elements), options);
     this._matrix4 = new THREEMatrix4();
     this.object3D = object3D;
+
+    this.update();
   }
 
   update() {
+    if(!this.object3D) {
+      return;
+    }
     this.matrix.x = this.object3D.position.x;
     this.matrix.y = this.object3D.position.y;
     this.matrix.z = this.object3D.position.z;
