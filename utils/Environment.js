0692b0432b9889cf8b0aadd484e93ef8d5c3181b
dmmn <ddotmnn+git@gmail.com>
2016-12-20 23:08:04 +0000

    add mobile-detect

---
+++ utils/Environment.js
@@ -0,0 +1,11 @@
+import MobileDetect from "mobile-detect";
+
+const MOBILE_DETECT = new MobileDetect(window.navigator.userAgent);
+
+let mobile = !!MOBILE_DETECT.mobile();
+
+export default class Environment {
+  static get mobile() {
+    return mobile;
+  }
+}
