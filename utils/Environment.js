b2184a377b4c1de80ed9cae63a3b0d6a2fc14ede
dmmn <ddotmnn+git@gmail.com>
2016-12-21 01:14:39 +0000

    add mobile url test

--- utils/Environment.js
+++ utils/Environment.js
@@ -2,7 +2,7 @@ import MobileDetect from "mobile-detect";
 
 const MOBILE_DETECT = new MobileDetect(window.navigator.userAgent);
 
-let mobile = !!MOBILE_DETECT.mobile();
+let mobile = !!MOBILE_DETECT.mobile() || /\bmobile\b/.test(window.location.search);
 
 export default class Environment {
   static get mobile() {
