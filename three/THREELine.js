4bf003f5ef3d04af79b89e3e548db53ae175a8e1
dmmn <ddotmnn+git@gmail.com>
2017-01-10 20:06:36 +0000

    THREELine working

--- three/THREELine.js
+++ three/THREELine.js
@@ -4,12 +4,12 @@ import THREEShaderMaterial from "./THREEShaderMaterial.js";
 
 const LEFT = new Vector3(-1, 0, 0);
 
-const DEBUG = true;
+const DEBUG = false;
 
 export default class THREELine extends Mesh {
   constructor({
     points,
-    material = new THREEShaderMaterial({type: "normal"}),
+    material = new THREEShaderMaterial({type : "normal"}),
     detail = 3,
     thickness = .1
   } = {}) {
@@ -20,13 +20,11 @@ export default class THREELine extends Mesh {
     this.points = points;
     this.frustumCulled = false;
 
-    this._tangent = new Vector3();
-    this._quaternion = new Quaternion();
     this._vector3 = new Vector3();
+    this._matrix4 = new Matrix4();
+    this._tangent = new Vector3();
     this._normal = new Vector3();
     this._binormal = new Vector3();
-    this._previousNormal = new Vector3();
-    this._matrix4 = new Matrix4();
 
     let positions = this.geometry.getAttribute("position").array;
     let verticesNumber = positions.length / 3;
@@ -47,94 +45,28 @@ export default class THREELine extends Mesh {
         ["start", `
           uniform float thickness;
           uniform vec3 positions[${this.points.length}];
-          uniform vec3 binormals[${this.points.length}];
           uniform vec3 normals[${this.points.length}];
 
           attribute float id;
-
-          mat3 matrixFromEuler(vec3 euler) {
-            mat3 m;
-
-            float a = cos(euler.x);
-            float b = sin(euler.x);
-            float c = cos(euler.y);
-            float d = sin(euler.y);
-            float e = cos(euler.z);
-            float f = sin(euler.z);
-
-            float ae = a * e;
-            float af = a * f;
-            float be = b * e;
-            float bf = b * f;
-
-            m[0][0] = c * e;
-            m[0][1] = - c * f;
-            m[0][2] = d;
-
-            m[1][0] = af + be * d;
-            m[1][1] = ae - bf * d;
-            m[1][2] = - b * c;
-
-            m[2][0] = bf - ae * d;
-            m[2][1] = be + af * d;
-            m[2][2] = a * c;
-
-            return m;
-          }
-
-          vec3 rotate_vector( vec4 q, vec3 vec ) {
-
-            float x = vec.x, y = vec.y, z = vec.z;
-        		float qx = q.x, qy = q.y, qz = q.z, qw = q.w;
-
-        		// calculate quat * vector
-
-        		float ix =  qw * x + qy * z - qz * y;
-        		float iy =  qw * y + qz * x - qx * z;
-        		float iz =  qw * z + qx * y - qy * x;
-        		float iw = - qx * x - qy * y - qz * z;
-
-        		// calculate result * inverse quat
-
-        		vec.x = ix * qw + iw * - qx + iy * - qz - iz * - qy;
-        		vec.y = iy * qw + iw * - qy + iz * - qx - ix * - qz;
-        		vec.z = iz * qw + iw * - qz + ix * - qy - iy * - qx;
-
-            return vec;
-            // return vec + 2.0 * cross( cross( vec, quat.xyz ) + quat.w * vec, quat.xyz );
-          }
         `],
         ["main", `
-          /// GLSL only
-
-          // vec3 offset = vec3(position.x, 0., position.z);
-          // vec3 position = positions[int(id)];
-          // vec3 nextPosition = positions[int(id + 1.)];
-          // vec3 ref = normalize(positions[1] - positions[0]);
-          // ref = ref.zxy;
-          // vec3 T = normalize(nextPosition - position);
-          // vec3 B = normalize(cross(T, ref));
-          // vec3 N = normalize(cross(B, T));
-          // vec3 normal = B * offset.x + N * offset.z;
-          // position = position + normal * thickness;
-
-          /// GLSL only
-
-          // mat3 rotationMatrix = matrixFromEuler(rotations[int(id)]);
-          // position *= rotationMatrix;
-
-
           vec3 position = position;
-          vec3 offset = vec3(position.x, 0., position.z);
-          vec3 normal = binormals[int(id)] * offset.x + normals[int(id)] * offset.z;
-          // normal.y = 0.;
-          position = positions[int(id)] + normal * thickness;
+          vec3 offset = position;
 
-          // position = offset * thickness;
+          position = positions[int(id)];
 
-          // position = rotate_vector(rotations[int(id)], position);
+          vec3 direction = normalize(mix(position - positions[int(id) - 1], positions[int(id) + 1] - position, step(1., ${this.points.length - 1}. - id)));
 
-          // position += positions[int(id)];
+          vec3 normal = normals[int(id)];
+
+          vec3 binormal = cross(normal, direction);
+
+          normal = normal * offset.x + binormal * offset.z;
+          // normal = normalize(normal);
+
+          // normal = normals[int(id)];
+
+          position += normal * thickness;
         `]
       ]
     });
@@ -142,7 +74,8 @@ export default class THREELine extends Mesh {
     if(DEBUG) {
       this._axisHelpers = [];
       for (let i = 0; i < this.points.length - 1; i++) {
-        let axisHelper = new AxisHelper(1);
+        let axisHelper = new AxisHelper(.2);
+        axisHelper.matrixAutoUpdate = false;
         this.add(axisHelper);
         this._axisHelpers.push(axisHelper);
       }
@@ -163,50 +96,41 @@ export default class THREELine extends Mesh {
         this._vector3.copy(this.points[i + 1]).add(point);
         this._vector3.normalize();
         [this._vector3.x, this._vector3.y, this._vector3.z] = [this._vector3.z, this._vector3.x, this._vector3.y];
-        this._binormal.crossVectors(this._tangent, this._vector3).normalize();
+        this._normal.crossVectors(this._tangent, this._vector3).normalize();
       } else {
-        this._binormal.crossVectors(this._tangent, this._previousNormal).normalize();
+        this._normal.crossVectors(this._tangent, this._binormal).normalize();
       }
 
-      this._normal.crossVectors(this._binormal, this._tangent).normalize();
+      this._binormal.crossVectors(this._normal, this._tangent).normalize();
 
-      this._matrix4.set(
-        this._binormal.x,
-        this._tangent.x,
-        this._normal.x,
-        point.x,
-
-        this._binormal.y,
-        this._tangent.y,
-        this._normal.y,
-        point.y,
-
-        this._binormal.z,
-        this._tangent.z,
-        this._normal.z,
-        point.z,
-
-        0,
-        0,
-        0,
-        1
-      );
-
-      this._quaternion.setFromRotationMatrix(this._matrix4);
-
-      this.material.normals[i] = this._normal;
-      this.material.binormals[i] = this._binormal;
+      this.material.normals[i].copy(this._normal);
       if (i === this.points.length - 2) {
-        this.material.normals[i + 1] = this._normal;
-        this.material.binormals[i + 1] = this._binormal;
+        this.material.normals[i + 1].copy(this._normal);
       }
 
       if(DEBUG) {
-        this._axisHelpers[i].matrixAutoUpdate = false;
-        this._axisHelpers[i].matrix.copy(this._matrix4);
-      }
+        this._axisHelpers[i].matrix.set(
+          this._normal.x,
+          this._tangent.x,
+          this._binormal.x,
+          point.x,
 
-      this._previousNormal.copy(this._normal);
+          this._normal.y,
+          this._tangent.y,
+          this._binormal.y,
+          point.y,
+
+          this._normal.z,
+          this._tangent.z,
+          this._binormal.z,
+          point.z,
+
+          0,
+          0,
+          0,
+          1
+        );
+      }
     }
   }
 }
