20d3475fff7ecb5a004c2d628ff92aced7174a22
dmmn <ddotmnn+git@gmail.com>
2017-01-19 19:41:04 +0000

    separate bumpFromDepth and bumpFromPackedDepth

--- shaders/DepthGLSL.js
+++ shaders/DepthGLSL.js
@@ -29,6 +29,25 @@ export default class DepthGLSL {
 
   static bumpFromDepth() {
     return `
+      vec4 bumpFromDepth(sampler2D texture, vec2 uv, vec2 resolution) {
+        vec2 size = vec2(.5, .0);
+        vec2 offset = vec2(.5, 0.);
+
+        float s11 = texture2D(texture, uv).r;
+        float s01 = texture2D(texture, uv + -offset.xy / resolution).r;
+        float s21 = texture2D(texture, uv + offset.xy / resolution).r;
+        float s10 = texture2D(texture, uv - offset.yx / resolution).r;
+        float s12 = texture2D(texture, uv + offset.yx / resolution).r;
+        vec3 va = normalize(vec3(size.xy / resolution, s21 - s01));
+        vec3 vb = normalize(vec3(size.yx / resolution, s12 - s10));
+        vec4 bump = vec4(cross(va,vb), s11);
+        return bump;
+      }
+    `;
+  }
+
+  static bumpFromPackedDepth() {
+    return `
       vec4 bumpFromDepth(sampler2D texture, vec2 uv, float scale) {
         vec2 size = vec2(.002, .0) * scale;
         vec3 off = vec3(-.001, 0., .001) * scale;
