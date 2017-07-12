bf4614442f15addcb530a313a0724ba856f68b1d
dmmn <ddotmnn+git@gmail.com>
2017-07-12 12:54:25 +0100

    add once option to signals

--- utils/Signal.js
+++ utils/Signal.js
@@ -1,7 +1,26 @@
 export default class Signal extends Set {
+  constructor() {
+    super();
+    
+    this._onceCallbacks = new Set();
+  }
+
+  add(value, {once = false} = {}) {
+    if(once) {
+      this._onceCallbacks.add(value);
+    }
+
+    super.add(value);
+  }
+
   dispatch(value) {
     for (let callback of this) {
       callback(value);
+
+      if(this._onceCallbacks.has(callback)) {
+        this._onceCallbacks.delete(callback);
+        this.delete(callback);
+      }
     }
   }
 }
