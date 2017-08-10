26b902a60fe3090b288d495eb2778578ffcb49cb
dmmn <ddotmnn+git@gmail.com>
2017-08-10 17:00:51 +0100

    add WebGLVideoElement

---
+++ customelements/WebGLVideoElement.js
@@ -0,0 +1,133 @@
+import LoopElement from "./LoopElement.js";
+import Ticker from "../utils/Ticker.js";
+import GLMesh from "../gl/GLMesh.js";
+import GLProgram from "../gl/GLProgram.js";
+import GLTexture from "../gl/GLTexture.js";
+
+(() => {
+  let style = document.createElement("style");
+  style.textContent = `
+    dlib-webglvideo {
+      display: block;
+    }
+  `;
+  document.head.appendChild(style);
+})();
+
+export default class WebGLVideoElement extends LoopElement {
+  constructor() {
+    super({
+      autoplay: false
+    });
+
+    this._video = document.createElement("video");
+    this._canvas = document.createElement("canvas");
+    this.appendChild(this._canvas);
+    this.appendChild(this._video);
+    this._video.loop = true;
+    this.gl = this._canvas.getContext("webgl", {
+      alpha: false
+    });
+
+    this._mesh = new GLMesh({
+      gl: this.gl,
+      attributes: [
+        ["position", {
+          data: new Float32Array([
+            -1, -1,
+            -1, 1,
+            1, -1,
+            1, 1
+          ]),
+          size: 2
+        }],
+        ["uv", {
+          data: new Float32Array([
+            0, 1,
+            0, 0,
+            1, 1,
+            1, 0
+          ]),
+          size: 2
+        }]
+      ]
+    });
+
+    this._videoTexture = new GLTexture({
+      gl: this.gl
+    });
+    this._videoTexture.minFilter = this.gl.LINEAR;
+    this._videoTexture.wrapS = this._videoTexture.wrapT = this.gl.CLAMP_TO_EDGE;
+
+    this.program = new GLProgram({
+      gl: this.gl,
+      uniforms: [
+        ["video", this._videoTexture]
+      ],
+      vertexShader: `
+        attribute vec2 position;
+        attribute vec2 uv;
+        varying vec2 vUv;
+
+        void main() {
+          gl_Position = vec4(position, 0., 1.);
+          vUv = uv;
+        }
+      `,
+      fragmentShader: `
+        precision highp float;
+
+        uniform sampler2D video;
+        varying vec2 vUv;
+
+        void main() {
+          gl_FragColor = texture2D(video, vUv);
+        }
+      `
+    });
+    this.program.use();
+
+    this.program.attributes.set(this._mesh.attributes);
+
+    this.src = this.getAttribute("src");
+  }
+
+  update() {
+    this.gl.viewport(0, 0, this.gl.drawingBufferWidth, this.gl.drawingBufferHeight);
+    this._videoTexture.source = this._video;
+    this._mesh.draw({
+      mode: this.gl.TRIANGLE_STRIP,
+      count: 4
+    });
+  }
+
+  get src() {
+    return this._video.src;
+  }
+
+  set src(value) {
+    this._video.src = value;
+
+    const resizeCanvas = () => {
+      this._video.removeEventListener("loadedmetadata", resizeCanvas);
+
+      this._canvas.width = Math.min(this._video.videoWidth, this.offsetWidth);
+      this._canvas.height = Math.min(this._video.videoHeight, this.offsetHeight);
+      //tmp
+      this._video.play();
+      this.play();
+    }
+
+    this._video.addEventListener("loadedmetadata", resizeCanvas);
+  }
+
+  set loop(value) {
+    this._video.loop = value;
+  }
+
+  get loop() {
+    return this._video.loop;
+  }
+}
+
+window.customElements.define("dlib-webglvideo", WebGLVideoElement);
