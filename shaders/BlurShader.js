910ddd1591e31e639ccf472840ea781a4698ef46
Damien Mortini <ddotmnn+git@gmail.com>
2017-11-26 19:54:09 +1100

    BlurShader compatible with WebGL2

--- shaders/BlurShader.js
+++ shaders/BlurShader.js
@@ -1,11 +1,36 @@
 // From https://github.com/Jam3/glsl-fast-gaussian-blur
 
-import Shader from "../3d/Shader.js";
+export default class BlurShader {
+  static shader({
+    textureName = "blurTexture",
+    textureUnit = 0,
+    distance = [1, 1]
+  } = {}) {
+    return {
+      uniforms: [
+        ["blurDistance", distance],
+        [textureName, textureUnit]
+      ],
+      vertexShaderChunks: [
+        ["start", `
+          uniform vec2 blurDistance;
+          ${BlurShader.computeBlurTextureCoordinates()}
+        `],
+        ["end", `computeBlurTextureCoordinates(uv, blurDistance);`]
+      ],
+      fragmentShaderChunks: [
+        ["start", `
+          uniform sampler2D ${textureName};
+          ${BlurShader.blur()}
+        `],
+        ["end", `fragColor = blur(${textureName});`]
+      ]
+    }
+  }
 
-export default class BlurShader extends Shader {
   static computeBlurTextureCoordinates() {
     return `
-      varying vec2 blurTextureCoordinates[15];
+      out vec2 blurTextureCoordinates[15];
 
       void computeBlurTextureCoordinates(vec2 uv, vec2 distance) {
         blurTextureCoordinates[0] = uv + distance * -.028;
@@ -29,53 +54,27 @@ export default class BlurShader extends Shader {
 
   static blur() {
     return `
-      varying vec2 blurTextureCoordinates[15];
+      in vec2 blurTextureCoordinates[15];
 
-      vec4 blur(sampler2D texture) {
+      vec4 blur(sampler2D inTexture) {
         vec4 result = vec4(0.0);
-        result += texture2D(texture, blurTextureCoordinates[0]) * 0.0044299121055113265;
-        result += texture2D(texture, blurTextureCoordinates[1]) * 0.00895781211794;
-        result += texture2D(texture, blurTextureCoordinates[2]) * 0.0215963866053;
-        result += texture2D(texture, blurTextureCoordinates[3]) * 0.0443683338718;
-        result += texture2D(texture, blurTextureCoordinates[4]) * 0.0776744219933;
-        result += texture2D(texture, blurTextureCoordinates[5]) * 0.115876621105;
-        result += texture2D(texture, blurTextureCoordinates[6]) * 0.147308056121;
-        result += texture2D(texture, blurTextureCoordinates[7]) * 0.159576912161;
-        result += texture2D(texture, blurTextureCoordinates[8]) * 0.147308056121;
-        result += texture2D(texture, blurTextureCoordinates[9]) * 0.115876621105;
-        result += texture2D(texture, blurTextureCoordinates[10]) * 0.0776744219933;
-        result += texture2D(texture, blurTextureCoordinates[11]) * 0.0443683338718;
-        result += texture2D(texture, blurTextureCoordinates[12]) * 0.0215963866053;
-        result += texture2D(texture, blurTextureCoordinates[13]) * 0.00895781211794;
-        result += texture2D(texture, blurTextureCoordinates[14]) * 0.0044299121055113265;
+        result += texture(inTexture, blurTextureCoordinates[0]) * 0.0044299121055113265;
+        result += texture(inTexture, blurTextureCoordinates[1]) * 0.00895781211794;
+        result += texture(inTexture, blurTextureCoordinates[2]) * 0.0215963866053;
+        result += texture(inTexture, blurTextureCoordinates[3]) * 0.0443683338718;
+        result += texture(inTexture, blurTextureCoordinates[4]) * 0.0776744219933;
+        result += texture(inTexture, blurTextureCoordinates[5]) * 0.115876621105;
+        result += texture(inTexture, blurTextureCoordinates[6]) * 0.147308056121;
+        result += texture(inTexture, blurTextureCoordinates[7]) * 0.159576912161;
+        result += texture(inTexture, blurTextureCoordinates[8]) * 0.147308056121;
+        result += texture(inTexture, blurTextureCoordinates[9]) * 0.115876621105;
+        result += texture(inTexture, blurTextureCoordinates[10]) * 0.0776744219933;
+        result += texture(inTexture, blurTextureCoordinates[11]) * 0.0443683338718;
+        result += texture(inTexture, blurTextureCoordinates[12]) * 0.0215963866053;
+        result += texture(inTexture, blurTextureCoordinates[13]) * 0.00895781211794;
+        result += texture(inTexture, blurTextureCoordinates[14]) * 0.0044299121055113265;
         return result;
       }
     `
-  }
-
-  constructor(options = {}) {
-    super(options);
-    let textureName = options.textureName || "blurTexture";
-    this.add({
-      uniforms: {
-        blurDistance: options.distance,
-        [textureName]: options.texture
-      },
-      vertexShaderChunks: [
-        ["start", `
-          uniform vec2 blurDistance;
-          ${BlurShader.computeBlurTextureCoordinates()}
-        `],
-        ["end", `computeBlurTextureCoordinates(uv, blurDistance);`]
-      ],
-      fragmentShaderChunks: [
-        ["start", `
-          uniform sampler2D ${textureName};
-
-          ${BlurShader.blur()}
-        `],
-        ["end", `gl_FragColor = blur(${textureName});`]
-      ]
-    });
   }
 }
