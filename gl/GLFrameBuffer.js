5f0d27b061d474e8b206a3c80e3058bc395e8370
Damien Mortini <ddotmnn+git@gmail.com>
2017-11-26 19:52:40 +1100

    add GLFrameBuffer

---
+++ gl/GLFrameBuffer.js
@@ -0,0 +1,44 @@
+export default class GLFrameBuffer {
+  constructor({
+    gl, 
+    target = gl.FRAMEBUFFER,
+  } = {}) {
+    this.gl = gl;
+    this.target = target;
+
+    this.colorTextures = [];
+    this.depthTexture = null;
+    this.stencilTexture = null;
+
+    this._frameBuffer = this.gl.createFramebuffer();
+  }
+
+  attach({
+    texture,
+    attachment = this.gl.COLOR_ATTACHMENT0,
+    target = this.target,
+    textarget = this.gl.TEXTURE_2D,
+  }) {
+    this.bind({target});
+    if(attachment === this.gl.DEPTH_ATTACHMENT) {
+      this.depthTexture = texture;
+    } else if(attachment === this.gl.STENCIL_ATTACHMENT) {
+      this.stencilTexture = texture;
+    } else {
+      this.colorTextures[attachment - this.gl.COLOR_ATTACHMENT0] = texture;
+    }
+    this.gl.framebufferTexture2D(target, attachment, textarget, texture._texture || texture, 0);
+  }
+
+  bind({
+    target = this.target
+  } = {}) {
+    this.gl.bindFramebuffer(target, this._frameBuffer);
+  }
+
+  unbind({
+    target = this.target
+  } = {}) {
+    this.gl.bindFramebuffer(target, null);
+  }
+};
