cef770cf2425a69bb6d180ada32a2d59e644d25e
Damien Mortini <ddotmnn+git@gmail.com>
2017-12-01 20:13:11 +1100

    improve APIs loading

--- api/SoundCloudAPI.js
+++ api/SoundCloudAPI.js
@@ -1,13 +1,13 @@
-export default class GoogleAPI {
+export default class SoundCloudAPI {
   static load() {
     return new Promise((resolve) => {
-      if(document.querySelector(`script[src$="//apis.google.com/js/api.js"]`)) {
+      if(document.querySelector(`script[src$="//w.soundcloud.com/player/api.js"]`)) {
         resolve();
       }
     
       let script = document.createElement("script");
       script.onload = resolve;
-      script.src = "//apis.google.com/js/api.js";
+      script.src = "//w.soundcloud.com/player/api.js";
       document.head.appendChild(script);
     });;
   }
--- api/YouTubeAPI.js
+++ api/YouTubeAPI.js
@@ -1,19 +1,17 @@
-const PROMISE = new Promise((resolve) => {
-  if(document.querySelector(`script[src$="//www.youtube.com/iframe_api"]`)) {
-    resolve();
-  }
-
-  window.onYouTubeIframeAPIReady = () => {
-    resolve();
-  }
-
-  let script = document.createElement("script");
-  script.src = "//www.youtube.com/iframe_api";
-  document.head.appendChild(script);
-});
-
 export default class YouTubeAPI {
-  static get ready() {
-    return PROMISE;
+  static load() {
+    return new Promise((resolve) => {
+      if(document.querySelector(`script[src$="//www.youtube.com/iframe_api"]`)) {
+        resolve();
+      }
+    
+      window.onYouTubeIframeAPIReady = () => {
+        resolve();
+      }
+    
+      let script = document.createElement("script");
+      script.src = "//www.youtube.com/iframe_api";
+      document.head.appendChild(script);
+    });;
   }
 }
