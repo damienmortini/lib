e17ca6049f45c18e870056e6a0d83fe50919cfe3
Damien Mortini <ddotmnn+git@gmail.com>
2017-12-03 03:45:36 +1100

    update GLTexture to allow TEXTURE_CUBE_MAP

--- gl/GLTexture.js
+++ gl/GLTexture.js
@@ -4,6 +4,7 @@ export default class GLTexture {
     data = null, 
     width = undefined,
     height = undefined,
+    target = (data && data.length) ? gl.TEXTURE_CUBE_MAP : gl.TEXTURE_2D,
     level = 0,
     internalformat = gl.RGBA,
     format = gl.RGBA,
@@ -17,6 +18,7 @@ export default class GLTexture {
     this._texture = this.gl.createTexture();
     this._width = width;
     this._height = height;
+    this._target = target;
     
     this.level = level;
     this.internalformat = internalformat;
@@ -33,19 +35,28 @@ export default class GLTexture {
 
   generateMipmap() {
     this.bind();
-    this.gl.generateMipmap(this.gl.TEXTURE_2D);
+    this.gl.generateMipmap(this._target);
     this.unbind();
   }
 
   set data(value) {
     this._data = value;
+
+    const data = (this._data && this._data.length) ? this._data : [this._data];
+
+    const width = this._width || data[0].width || data[0].videoWidth;
+    const height = this._height || data[0].height || data[0].videoHeight;
+    
+    const count = this._target === this.gl.TEXTURE_CUBE_MAP ? 6 : 1;
+    const target = this._target === this.gl.TEXTURE_CUBE_MAP ? this.gl.TEXTURE_CUBE_MAP_POSITIVE_X : this._target;
+
     this.bind();
-    const width = this._width || this._data.width || this._data.videoWidth;
-    const height = this._height || this._data.height || this._data.videoHeight;
-    if((this.gl instanceof WebGLRenderingContext) && (this._data.width || this._data.videoWidth)) {
-      this.gl.texImage2D(this.gl.TEXTURE_2D, this.level, this.internalformat, this.format, this.type, this._data);
-    } else {
-      this.gl.texImage2D(this.gl.TEXTURE_2D, this.level, this.internalformat, width, height, 0, this.format, this.type, this._data);
+    for (let i = 0; i < data.length; i++) {
+      if((this.gl instanceof WebGLRenderingContext) && (data[0].width || data[0].videoWidth)) {
+        this.gl.texImage2D(target + i, this.level, this.internalformat, this.format, this.type, data[i]);
+      } else {
+        this.gl.texImage2D(target + i, this.level, this.internalformat, width, height, 0, this.format, this.type, data[i]);
+      }
     }
     this.unbind();
   }
@@ -68,7 +79,7 @@ export default class GLTexture {
     }
     this._minFilter = value;
     this.bind();
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this._minFilter);
+    this.gl.texParameteri(this._target, this.gl.TEXTURE_MIN_FILTER, this._minFilter);
     this.unbind();
   }
 
@@ -82,7 +93,7 @@ export default class GLTexture {
     }
     this._magFilter = value;
     this.bind();
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this._magFilter);
+    this.gl.texParameteri(this._target, this.gl.TEXTURE_MAG_FILTER, this._magFilter);
     this.unbind();
   }
 
@@ -96,7 +107,7 @@ export default class GLTexture {
     }
     this._wrapS = value;
     this.bind();
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this._wrapS);
+    this.gl.texParameteri(this._target, this.gl.TEXTURE_WRAP_S, this._wrapS);
     this.unbind();
   }
 
@@ -110,7 +121,7 @@ export default class GLTexture {
     }
     this._wrapT = value;
     this.bind();
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this._wrapT);
+    this.gl.texParameteri(this._target, this.gl.TEXTURE_WRAP_T, this._wrapT);
     this.unbind();
   }
 
@@ -120,10 +131,10 @@ export default class GLTexture {
 
   bind({unit = 0} = {}) {
     this.gl.activeTexture(this.gl[`TEXTURE${unit}`]);
-    this.gl.bindTexture(this.gl.TEXTURE_2D, this._texture);
+    this.gl.bindTexture(this._target, this._texture);
   }
 
   unbind() {
-    this.gl.bindTexture(this.gl.TEXTURE_2D, null);
+    this.gl.bindTexture(this._target, null);
   }
 };
