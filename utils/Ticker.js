1f1b81f8d2d72665f9e53eb01a0f6e53bbb15199
Damien Mortini <ddotmnn+github@gmail.com>
2018-02-05 13:22:36 +1100

    remove performance.now() fallback

--- utils/Ticker.js
+++ utils/Ticker.js
@@ -6,7 +6,7 @@ class Ticker extends Signal {
     
     this._updateBinded = this.update.bind(this);
 
-    this._previousTimestamp = 0;
+    this._previousTimestamp = window.performance.now();
     this.deltaTime = 0;
     this.timeScale = 1;
 
@@ -16,7 +16,7 @@ class Ticker extends Signal {
   update(time) {
     requestAnimationFrame(this._updateBinded);
 
-    let timestamp = window.performance ? window.performance.now() : Date.now();
+    const timestamp = window.performance.now();
     this.deltaTime = (timestamp - this._previousTimestamp) * .001;
     this.timeScale = this.deltaTime / .0166666667;
     this._previousTimestamp = timestamp;
