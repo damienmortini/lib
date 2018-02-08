4c7b76ff836e71ac069a285999aaa6fa04888b9e
Damien Mortini <ddotmnn+github@gmail.com>
2018-02-08 16:02:31 +1100

    add styleToRGBA in Color

--- math/Color.js
+++ math/Color.js
@@ -1,8 +1,30 @@
-export function hexToRGB(hex) {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? [
-    parseInt(result[1], 16) / 255,
-    parseInt(result[2], 16) / 255,
-    parseInt(result[3], 16) / 255
-   ] : null;
+let ghostElement;
+
+export default class Color {
+  static styleToRGBA(value) {
+    if(!ghostElement) {
+      ghostElement = document.createElement("span");
+      ghostElement.id = "dlib-color-ghost-element";
+      document.head.appendChild(ghostElement);
+    }
+  
+    ghostElement.style.color = value;
+
+    const results = /rgba?\s*\(\s*(\d*),\s*(\d*)\s*,\s*(\d*)\s*(,\s*([\.\d]*))?\s*\)/.exec(getComputedStyle(ghostElement).getPropertyValue("color"));
+    return results ? [
+      parseInt(results[1]) / 255,
+      parseInt(results[2]) / 255,
+      parseInt(results[3]) / 255,
+      results[5] !== undefined ? parseFloat(results[5]) : 1
+     ] : null;
+  }
+
+  static hexToRGB(hex) {
+    var results = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return results ? [
+      parseInt(results[1], 16) / 255,
+      parseInt(results[2], 16) / 255,
+      parseInt(results[3], 16) / 255
+     ] : null;
+  }
 }
