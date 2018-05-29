92f1f41e4fb611c2f89e94574b1ae77207f70ab1
Damien Mortini <ddotmnn+github@gmail.com>
2018-05-29 14:55:05 +1000

    wrap once callbacks in signal for performances

--- utils/Signal.js
+++ utils/Signal.js
@@ -1,26 +1,31 @@
 export default class Signal extends Set {
   constructor() {
     super();
-    
-    this._onceCallbacks = new Set();
+
+    this._onceCallbacksMap = new Map();
   }
 
   add(value, {once = false} = {}) {
     if(once) {
-      this._onceCallbacks.add(value);
+      const onceCallbackWrapper = () => {
+        value(...arguments);
+        this.delete(value);
+      };
+      this._onceCallbacksMap.set(value, onceCallbackWrapper);
+      super.add(onceCallbackWrapper);
+    } else {
+      super.add(value);
     }
+  }
 
-    super.add(value);
+  delete(value) {
+    super.delete(this._onceCallbacksMap.get(value) || value);
+    this._onceCallbacksMap.delete(value);
   }
 
   dispatch(value) {
     for (let callback of this) {
       callback(value);
-
-      if(this._onceCallbacks.has(callback)) {
-        this._onceCallbacks.delete(callback);
-        this.delete(callback);
-      }
     }
   }
 }
