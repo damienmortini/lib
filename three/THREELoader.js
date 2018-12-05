ded27c6b407f96718ba4240668379f5ac56672e1
Damien Mortini <ddotmnn+github@gmail.com>
2018-12-05 21:06:36 +1000

    add THREEGLTFLoader and remove global THREE refs in THREELoader

--- three/THREELoader.js
+++ three/THREELoader.js
@@ -1,12 +1,14 @@
-import "../../three/examples/js/loaders/GLTFLoader.js";
 import { Loader } from "../utils/Loader.js";
+import { TextureLoader } from "../../three/src/loaders/TextureLoader.js";
+
+import THREEGLTFLoader from "./THREEGLTFLoader.js";
 
 function scaleScene(data, scale) {
   if (scale === 1) {
     return;
   }
   data.traverse((object3D) => {
-    if (object3D instanceof THREE.Mesh || object3D instanceof THREE.Line || object3D instanceof THREE.LineSegments) {
+    if (object3D instanceof Mesh || object3D instanceof Line || object3D instanceof LineSegments) {
       object3D.geometry.scale(scale, scale, scale);
     }
     object3D.position.multiplyScalar(scale);
@@ -18,14 +20,17 @@ class THREELoader extends Loader {
   _loadFile(src, { scale = 1 } = {}) {
     return new Promise((resolve) => {
       if (/\.(gltf|glb)$/.test(src)) {
-        const loader = new THREE.GLTFLoader();
-        loader.setPath(/(.*[\/\\]).*$/.exec(src)[1]);
-        loader.load(src, (data) => {
+        const loader = new THREEGLTFLoader();
+
+        const [, path, file] = /(.*[\/\\])(.*$)/.exec(src);
+
+        loader.setPath(path);
+        loader.load(file, (data) => {
           scaleScene(data.scene, scale);
           resolve(data);
         });
       } else if (/\.(png|jpg|mp4)$/.test(src)) {
-        new THREE.TextureLoader().load(src, (data) => {
+        new TextureLoader().load(src, (data) => {
           resolve(data);
         });
       }
