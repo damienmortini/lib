4241b5fd0c878d33bf2e8c193b940a9893b1ab87
Damien Mortini <ddotmnn+git@gmail.com>
2019-01-15 15:58:42 +0100

    add texture options to THREEWebGLRenderTarget2D

--- packages/three/renderer/THREEWebGLRenderTarget2D.js
+++ packages/three/renderer/THREEWebGLRenderTarget2D.js
@@ -7,27 +7,52 @@ import { OrthographicCamera } from "../../three/src/cameras/OrthographicCamera.j
 import THREEShaderMaterial from "../material/THREEShaderMaterial.js";
 
 export default class THREEWebGLRenderTarget2D extends WebGLRenderTarget {
-	constructor({
-		material,
-		renderer,
-		width = 1024,
-		height = 1024,
-	}) {
-		super(width, height, {});
+  constructor({
+    material,
+    renderer,
+    width = 1024,
+    height = 1024,
+    wrapS,
+    wrapT,
+    magFilter,
+    minFilter,
+    format,
+    type,
+    anisotropy,
+    encoding,
+    generateMipmaps,
+    depthBuffer,
+    stencilBuffer,
+    depthTexture,
+  }) {
+    super(width, height, {
+      wrapS,
+      wrapT,
+      magFilter,
+      minFilter,
+      format,
+      type,
+      anisotropy,
+      encoding,
+      generateMipmaps,
+      depthBuffer,
+      stencilBuffer,
+      depthTexture,
+    });
 
-		this.material = material;
-		this.renderer = renderer;
+    this.material = material;
+    this.renderer = renderer;
 
-		this._scene = new Scene();
-		this._camera = new OrthographicCamera(-1, 1, 1, -1, 0, 1);
-		
-		this._quad = new Mesh(new PlaneBufferGeometry(2, 2), material);
-		this._scene.add(this._quad);
+    this._scene = new Scene();
+    this._camera = new OrthographicCamera(-1, 1, 1, -1, 0, 1);
 
-		this.render();
-	}
+    this._quad = new Mesh(new PlaneBufferGeometry(2, 2), material);
+    this._scene.add(this._quad);
 
-	render() {
-		this.renderer.render(this._scene, this._camera, this);
-	}
+    this.render();
+  }
+
+  render() {
+    this.renderer.render(this._scene, this._camera, this);
+  }
 }
