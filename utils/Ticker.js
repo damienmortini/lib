8bcfee46478a7f552d009fef2c2ef2a3ca066e07
Damien Mortini <ddotmnn+git@gmail.com>
2019-01-30 17:46:23 +0100

    add smoothDeltaTime - smoothTimeScale to Ticker

--- utils/Ticker.js
+++ utils/Ticker.js
@@ -1,5 +1,7 @@
 import Signal from "./Signal.js";
 
+const DELTA_TIME_BASE = 1 / 60;
+
 class Ticker extends Signal {
   constructor() {
     super();
@@ -8,8 +10,10 @@ class Ticker extends Signal {
 
     this.time = window.performance.now() * .001;
     this._previousTime = this.time;
-    this.deltaTime = 0;
+    this.deltaTime = DELTA_TIME_BASE;
     this.timeScale = 1;
+    this.smoothDeltatime = this.deltaTime;
+    this.smoothTimeScale = this.timeScale;
 
     this._update();
   }
@@ -19,7 +23,9 @@ class Ticker extends Signal {
 
     this.time = window.performance.now() * 0.001;
     this.deltaTime = this.time - this._previousTime;
-    this.timeScale = this.deltaTime / .0166666667;
+    this.smoothDeltatime += (this.deltaTime - this.smoothDeltatime) * .05;
+    this.timeScale = this.deltaTime / DELTA_TIME_BASE;
+    this.smoothTimeScale = this.smoothDeltatime / DELTA_TIME_BASE;
     this._previousTime = this.time;
 
     this.dispatch();
