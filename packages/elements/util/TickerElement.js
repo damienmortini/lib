757b8a11a53f8e2ff67678df6dc0d46b109290e1
Damien Mortini <ddotmnn+git@gmail.com>
2019-02-14 21:40:12 +0100

    refactor and simplify TickerElement

--- packages/elements/util/TickerElement.js
+++ packages/elements/util/TickerElement.js
@@ -8,33 +8,44 @@ export default class TickerElement extends HTMLElement {
     this._background = background || this.hasAttribute("background");
 
     this._paused = true;
-    this.__pausedByBlur = false;
+    this._pausedByUser = true;
+    this._pausedByBlur = false;
 
     this._updateBinded = this.update.bind(this);
+    this._onFocusChangeBinded = this._onFocusChange.bind(this);
   }
 
   connectedCallback() {
     if (!this._background) {
-      window.top.addEventListener("blur", this._onBlur = () => {
-        this._pausedByBlur = true;
-      });
-      window.top.addEventListener("focus", this._onFocus = () => {
-        this._pausedByBlur = false;
-      });
+      window.top.addEventListener("blur", this._onFocusChangeBinded);
+      window.top.addEventListener("focus", this._onFocusChangeBinded);
     }
     if (this._autoplay) {
-      this.play();
       if (!window.top.document.hasFocus() && !this._background) {
         this._pausedByBlur = true;
         requestAnimationFrame(this._updateBinded);
       }
+      this.play();
     }
   }
 
   disconnectedCallback() {
-    this.pause();
-    window.top.removeEventListener("blur", this._onBlur);
-    window.top.removeEventListener("focus", this._onFocus);
+    this._pausedByBlur = true;
+    window.top.removeEventListener("blur", this._onFocusChangeBinded);
+    window.top.removeEventListener("focus", this._onFocusChangeBinded);
+  }
+
+  get paused() {
+    return this._paused;
+  }
+
+  get _pausedByUser() {
+    return this.__pausedByUser;
+  }
+
+  set _pausedByUser(value) {
+    this.__pausedByUser = value;
+    this._updatePlaybackState();
   }
 
   get _pausedByBlur() {
@@ -42,45 +53,45 @@ export default class TickerElement extends HTMLElement {
   }
 
   set _pausedByBlur(value) {
-    if (value === this.__pausedByBlur) {
-      return;
-    }
     this.__pausedByBlur = value;
-    if (this.__pausedByBlur) {
-      Ticker.delete(this._updateBinded);
-    } else {
-      if (!this.paused) {
-        Ticker.add(this._updateBinded);
-      }
+    this._updatePlaybackState();
+  }
+
+  _onFocusChange(event) {
+    switch (event.type) {
+      case "blur":
+        this._pausedByBlur = true;
+        break;
+      case "focus":
+        this._pausedByBlur = false;
+        break;
     }
   }
 
-  get paused() {
-    return this._paused;
-  }
+  _updatePlaybackState() {
+    const paused = this._pausedByUser || this._pausedByBlur;
 
-  set paused(value) {
-    if (value === this._paused) {
+    if (paused === this._paused) {
       return;
     }
-    this._paused = value;
+
+    this._paused = paused;
+
     if (this._paused) {
       Ticker.delete(this._updateBinded);
       this.dispatchEvent(new Event("pause"));
     } else {
-      if (!this._pausedByBlur) {
-        Ticker.add(this._updateBinded);
-      }
+      Ticker.add(this._updateBinded);
       this.dispatchEvent(new Event("playing"));
     }
   }
 
   play() {
-    this.paused = false;
+    this._pausedByUser = false;
   }
 
   pause() {
-    this.paused = true;
+    this._pausedByUser = true;
   }
 
   update() { }
