43a8fdc4b65550feba163805cafc5923a25aad64
Damien Mortini <ddotmnn+git@gmail.com>
2019-07-02 18:50:28 +0200

    add NormalShader

---
+++ shader/NormalShader.js
@@ -0,0 +1,27 @@
+export default class NormalShader {
+  static perturbNormal() {
+    return `
+      vec3 perturbNormal(vec3 baseNormal, vec3 normal, vec3 viewPosition, vec2 uv) {
+        vec3 p = -normalize(viewPosition);
+
+        vec3 dp1 = dFdx(p);
+        vec3 dp2 = dFdy(p);
+        vec2 duv1 = dFdx(uv);
+        vec2 duv2 = dFdy(uv);
+  
+        // solve the linear system
+        vec3 dp2perp = cross(dp2, baseNormal);
+        vec3 dp1perp = cross(baseNormal, dp1);
+        vec3 T = dp2perp * duv1.x + dp1perp * duv2.x;
+        vec3 B = dp2perp * duv1.y + dp1perp * duv2.y;
+  
+        // construct a scale-invariant frame 
+        float invmax = 1.0 / sqrt(max(dot(T,T), dot(B,B)));
+        
+        mat3 TBN = mat3(normalize(T * invmax), normalize(B * invmax), baseNormal);
+  
+        return normalize(TBN * normal);
+      }
+    `;
+  }
+}
