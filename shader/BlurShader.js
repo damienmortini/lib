bd8a7de2f8417c91a10c8e894ddc0acf8610b1f7
Damien Mortini <ddotmnn+git@gmail.com>
2019-08-14 20:09:54 +0200

    simplify BlurShader

--- shader/BlurShader.js
+++ shader/BlurShader.js
@@ -1,85 +1,21 @@
 // From https://github.com/Jam3/glsl-fast-gaussian-blur
 
 export default class BlurShader {
-  constructor({
-    uvs = true,
-    uniforms = {},
-    vertexShaderChunks = [],
-    fragmentShaderChunks = [],
-  } = {}) {
-    return {
-      uniforms: Object.assign({
-        blurDistance: [1, 1],
-        blurTexture: 0,
-      }, uniforms),
-      vertexShaderChunks: [
-        ["start", `
-          uniform vec2 blurDistance;
-
-          ${uvs ? "in vec2 uv;" : ""}
-          
-          ${BlurShader.computeBlurTextureCoordinates()}
-        `],
-        ["end", "computeBlurTextureCoordinates(uv, blurDistance);"],
-        ...vertexShaderChunks,
-      ],
-      fragmentShaderChunks: [
-        ["start", `
-          uniform sampler2D blurTexture;
-          ${BlurShader.blur()}
-        `],
-        ["end", "fragColor = blur(blurTexture);"],
-        ...fragmentShaderChunks,
-      ],
-    };
-  }
-
-  static computeBlurTextureCoordinates() {
-    return `
-      out vec2 blurTextureCoordinates[15];
-
-      void computeBlurTextureCoordinates(vec2 uv, vec2 distance) {
-        blurTextureCoordinates[0] = uv + distance * -.028;
-        blurTextureCoordinates[1] = uv + distance * -.024;
-        blurTextureCoordinates[2] = uv + distance * -.020;
-        blurTextureCoordinates[3] = uv + distance * -.016;
-        blurTextureCoordinates[4] = uv + distance * -.012;
-        blurTextureCoordinates[5] = uv + distance * -.008;
-        blurTextureCoordinates[6] = uv + distance * -.004;
-        blurTextureCoordinates[7] = uv;
-        blurTextureCoordinates[8] = uv + distance * .004;
-        blurTextureCoordinates[9] = uv + distance * .008;
-        blurTextureCoordinates[10] = uv + distance * .012;
-        blurTextureCoordinates[11] = uv + distance * .016;
-        blurTextureCoordinates[12] = uv + distance * .020;
-        blurTextureCoordinates[13] = uv + distance * .024;
-        blurTextureCoordinates[14] = uv + distance * .028;
-      }
-    `;
-  }
-
   static blur() {
     return `
-      in vec2 blurTextureCoordinates[15];
-
-      vec4 blur(sampler2D inTexture) {
-        vec4 result = vec4(0.0);
-        result += texture(inTexture, blurTextureCoordinates[0]) * 0.0044299121055113265;
-        result += texture(inTexture, blurTextureCoordinates[1]) * 0.00895781211794;
-        result += texture(inTexture, blurTextureCoordinates[2]) * 0.0215963866053;
-        result += texture(inTexture, blurTextureCoordinates[3]) * 0.0443683338718;
-        result += texture(inTexture, blurTextureCoordinates[4]) * 0.0776744219933;
-        result += texture(inTexture, blurTextureCoordinates[5]) * 0.115876621105;
-        result += texture(inTexture, blurTextureCoordinates[6]) * 0.147308056121;
-        result += texture(inTexture, blurTextureCoordinates[7]) * 0.159576912161;
-        result += texture(inTexture, blurTextureCoordinates[8]) * 0.147308056121;
-        result += texture(inTexture, blurTextureCoordinates[9]) * 0.115876621105;
-        result += texture(inTexture, blurTextureCoordinates[10]) * 0.0776744219933;
-        result += texture(inTexture, blurTextureCoordinates[11]) * 0.0443683338718;
-        result += texture(inTexture, blurTextureCoordinates[12]) * 0.0215963866053;
-        result += texture(inTexture, blurTextureCoordinates[13]) * 0.00895781211794;
-        result += texture(inTexture, blurTextureCoordinates[14]) * 0.0044299121055113265;
-        return result;
+      vec4 blur(sampler2D texture, vec2 uv, vec2 resolution, vec2 direction) {
+        vec4 color = vec4(0.0);
+        vec2 off1 = vec2(1.411764705882353) * direction;
+        vec2 off2 = vec2(3.2941176470588234) * direction;
+        vec2 off3 = vec2(5.176470588235294) * direction;
+        color += texture2D(texture, uv) * 0.1964825501511404;
+        color += texture2D(texture, uv + (off1 / resolution)) * 0.2969069646728344;
+        color += texture2D(texture, uv - (off1 / resolution)) * 0.2969069646728344;
+        color += texture2D(texture, uv + (off2 / resolution)) * 0.09447039785044732;
+        color += texture2D(texture, uv - (off2 / resolution)) * 0.09447039785044732;
+        color += texture2D(texture, uv + (off3 / resolution)) * 0.010381362401148057;
+        color += texture2D(texture, uv - (off3 / resolution)) * 0.010381362401148057;
+        return color;
       }
     `;
   }
