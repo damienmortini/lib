5022a28880acb739cd6d0bdc5ae4c7d859b7d1f9
Damien Mortini <ddotmnn+git@gmail.com>
2019-09-06 16:34:17 +0200

    update GerstnerWavesShader

--- shader/GerstnerWavesShader.js
+++ shader/GerstnerWavesShader.js
@@ -1,8 +1,33 @@
 // From https://catlikecoding.com/unity/tutorials/flow/waves/
 
 export default class GerstnerWaveShader {
+  static get GerstnerWave() {
+    return `
+      struct GerstnerWave {
+        vec2 direction;
+        float wavenumber;
+        float amplitude;
+        float steepness;
+        float speed;
+      };
+    `;
+  }
+
   static gerstnerWave() {
     return `
+      vec3 gerstnerWave(vec2 position, vec2 direction, float wavenumber, float amplitude, float steepness, float speed, float time) {
+        float f = wavenumber * (dot(direction, position) - speed * time);
+
+        float cosf = cos(f);
+        float sinf = sin(f);
+        
+        return vec3(
+          steepness * amplitude * direction.x * cosf,
+          steepness * amplitude * sinf,
+          steepness * amplitude * direction.y * cosf
+        );
+      }
+
       vec3 gerstnerWave(vec2 position, vec2 direction, float wavenumber, float amplitude, float steepness, float speed, float time, inout vec3 normal) {
         float f = wavenumber * (dot(direction, position) - speed * time);
 
