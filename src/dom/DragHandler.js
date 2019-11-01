923dff15e7d9346ca859dd5871aad00462a0ffdb
Damien Mortini <ddotmnn+git@gmail.com>
2019-11-02 00:58:55 +0900

    use DOMMatrix in DragHandler

--- src/dom/DragHandler.js
+++ src/dom/DragHandler.js
@@ -7,23 +7,27 @@ export default class DragHandler extends Set {
 
     this.exceptions = exceptions;
 
+    this.selected = new Set();
+
     this._onPointerDownBinded = this._onPointerDown.bind(this);
     this._onPointerMoveBinded = this._onPointerMove.bind(this);
     this._onPointerUpBinded = this._onPointerUp.bind(this);
     this._updateBinded = this._update.bind(this);
 
+    this._previousClientX = 0;
+    this._previousClientY = 0;
+
+    this._clientX = 0;
+    this._clientY = 0;
+
     this._dragStartX = 0;
     this._dragStartY = 0;
-
-    this._dragX = 0;
-    this._dragY = 0;
 
     this._preventDrag = false;
 
     this._animationFrameID = -1;
 
-    this.selected = new Set();
-    this._elementBoundingClientRects = new Map();
+    this._elementTransformMatrices = new Map();
 
     this._resizeObserver = new ResizeObserver((entries) => {
       this._preventDrag = true;
@@ -44,13 +48,15 @@ export default class DragHandler extends Set {
       }
     }
     this._preventDrag = false;
+    this._previousClientX = event.clientX;
+    this._previousClientY = event.clientY;
+    this._clientX = event.clientX;
+    this._clientY = event.clientY;
     this._dragStartX = event.clientX;
     this._dragStartY = event.clientY;
-    this._dragX = event.clientX;
-    this._dragY = event.clientY;
     this.selected.add(event.currentTarget);
     for (const element of this.selected) {
-      this._elementBoundingClientRects.set(element, element.getBoundingClientRect());
+      this._elementTransformMatrices.set(element, new DOMMatrix(element.style.transform));
     }
     window.addEventListener('pointermove', this._onPointerMoveBinded, { passive: false });
     window.addEventListener('pointerup', this._onPointerUpBinded, { passive: false });
@@ -58,35 +64,18 @@ export default class DragHandler extends Set {
   }
 
   _onPointerMove(event) {
-    if (this._preventDrag) {
-      window.removeEventListener('pointermove', this._onPointerMoveBinded);
-      for (const element of this.selected) {
-        element.style.transform = '';
-        element.style.pointerEvents = '';
-      }
-      return;
-    }
-    if (Math.abs(event.clientX - this._dragStartX) < 2 || Math.abs(event.clientY - this._dragStartY) < 2) {
-      return;
-    }
     for (const element of this.selected) {
       element.style.pointerEvents = 'none';
     }
-    this._dragX = event.clientX;
-    this._dragY = event.clientY;
+    this._clientX = event.clientX;
+    this._clientY = event.clientY;
   }
 
-  _onPointerUp(event) {
+  _onPointerUp() {
     cancelAnimationFrame(this._animationFrameID);
     window.removeEventListener('pointermove', this._onPointerMoveBinded);
     window.removeEventListener('pointerup', this._onPointerUpBinded);
     for (const element of this.selected) {
-      const boundingClientRect = this._elementBoundingClientRects.get(element);
-      if (!this._preventDrag) {
-        element.style.left = `${boundingClientRect.left + event.clientX - this._dragStartX}px`;
-        element.style.top = `${boundingClientRect.top + event.clientY - this._dragStartY}px`;
-      }
-      element.style.transform = '';
       element.style.pointerEvents = '';
     }
     this._preventDrag = false;
@@ -94,10 +83,21 @@ export default class DragHandler extends Set {
   }
 
   _update() {
-    this._animationFrameID = requestAnimationFrame(this._updateBinded);
-    for (const element of this.selected) {
-      element.style.transform = `translate(${this._dragX - this._dragStartX}px, ${this._dragY - this._dragStartY}px)`;
+    if (this._preventDrag) {
+      this._onPointerUp();
+      return;
     }
+    this._animationFrameID = requestAnimationFrame(this._updateBinded);
+    if (Math.abs(this._clientX - this._dragStartX) < 2 || Math.abs(this._clientY - this._dragStartY) < 2) {
+      return;
+    }
+    for (const element of this.selected) {
+      const domMatrix = this._elementTransformMatrices.get(element);
+      domMatrix.translateSelf(this._clientX - this._previousClientX, this._clientY - this._previousClientY);
+      element.style.transform = domMatrix.toString();
+    }
+    this._previousClientX = this._clientX;
+    this._previousClientY = this._clientY;
   }
 
   add(element) {
