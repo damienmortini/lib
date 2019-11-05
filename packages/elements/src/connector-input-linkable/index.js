a851434482d291614a0777866ab9071e54af6715
Damien Mortini <ddotmnn+git@gmail.com>
2019-11-06 00:48:41 +0900

    fix connector for touch

--- packages/elements/src/connector-input-linkable/index.js
+++ packages/elements/src/connector-input-linkable/index.js
@@ -1,5 +1,7 @@
 import ConnectorInputElement from '../connector-input/index.js';
 
+const CONNECTORS = new Set();
+
 let activeConnector = null;
 
 /**
@@ -10,6 +12,11 @@ class ConnectorInputLinkableElement extends ConnectorInputElement {
     super();
 
     this.shadowRoot.querySelector('slot style').insertAdjacentHTML('beforeend', `
+      :host {
+        cursor: pointer;
+        margin: 0;
+        padding: .2em;
+      }
       :host(:hover) {
         background: red;
       }
@@ -24,6 +31,19 @@ class ConnectorInputLinkableElement extends ConnectorInputElement {
     this.addEventListener('pointerdown', this._onPointerDown);
 
     this._onWindowPointerUpBinded = this._onWindowPointerUp.bind(this);
+    this.addEventListener('pointerup', (event) => {
+      console.log(event.composedPath());
+    }, { passive: false });
+  }
+
+  connectedCallback() {
+    CONNECTORS.add(this);
+    console.log(CONNECTORS);
+  }
+
+  disconnectedCallback() {
+    CONNECTORS.delete(this);
+    super.disconnectedCallback();
   }
 
   _onPointerDown() {
@@ -32,7 +52,7 @@ class ConnectorInputLinkableElement extends ConnectorInputElement {
     }
     activeConnector = this;
 
-    window.addEventListener('pointerup', this._onWindowPointerUpBinded);
+    window.addEventListener('pointerup', this._onWindowPointerUpBinded, { passive: false });
 
     this.dispatchEvent(new CustomEvent('linkstart', {
       composed: true,
@@ -49,21 +69,25 @@ class ConnectorInputLinkableElement extends ConnectorInputElement {
       return;
     }
 
-    let connector;
-    for (const element of event.path) {
-      if (element instanceof ConnectorInputLinkableElement) {
-        connector = element;
-        break;
+    let hitConnector;
+
+    for (const connector of CONNECTORS) {
+      const boundingClientRect = connector.getBoundingClientRect();
+      if (event.clientX > boundingClientRect.x &&
+        event.clientX < boundingClientRect.x + boundingClientRect.width &&
+        event.clientY > boundingClientRect.y &&
+        event.clientY < boundingClientRect.y + boundingClientRect.height) {
+        hitConnector = connector;
       }
     }
 
-    if (connector === activeConnector) {
+    if (hitConnector === activeConnector) {
       return;
     }
 
     window.removeEventListener('pointerup', this._onWindowPointerUpBinded);
 
-    if (!connector || (activeConnector.type === connector.type && connector.type !== ConnectorInputLinkableElement.TYPE_BOTH)) {
+    if (!hitConnector || (activeConnector.type === hitConnector.type && hitConnector.type !== ConnectorInputLinkableElement.TYPE_BOTH)) {
       activeConnector = null;
       this.dispatchEvent(new CustomEvent('linkend', {
         composed: true,
@@ -76,8 +100,8 @@ class ConnectorInputLinkableElement extends ConnectorInputElement {
       return;
     }
 
-    const inputConnector = activeConnector.type & ConnectorInputLinkableElement.TYPE_OUTPUT ? activeConnector : connector;
-    const outputConnector = connector.type & ConnectorInputLinkableElement.TYPE_INPUT ? connector : activeConnector;
+    const inputConnector = activeConnector.type & ConnectorInputLinkableElement.TYPE_OUTPUT ? activeConnector : hitConnector;
+    const outputConnector = hitConnector.type & ConnectorInputLinkableElement.TYPE_INPUT ? hitConnector : activeConnector;
 
     inputConnector.outputs.add(outputConnector);
 
