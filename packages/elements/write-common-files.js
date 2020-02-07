baf0167200769a620d937fadc0f30fab270c8653
Damien Mortini <ddotmnn+git@gmail.com>
2020-02-07 19:43:00 +0100

    clean and update packages data

--- packages/elements/write-common-files.js
+++ packages/elements/write-common-files.js
@@ -1,22 +1,33 @@
 const fs = require('fs');
 
 for (const dirent of fs.readdirSync('packages/', { withFileTypes: true })) {
-  fs.writeFileSync(`packages/${dirent.name}/package.json`, `{
-  "name": "@damienmortini/${dirent.name}",
-  "version": "0.0.1",
-  "description": "<${dirent.name}> custom element.",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \\"Error: no test specified\\" && exit 1"
-  },
-  "author": "Damien Mortini",
-  "license": "ISC",
-  "repository": {
-    "type": "git",
-    "url": "ssh://git@github.com/damienmortini/elements.git",
-    "directory": "packages/${dirent.name}"
-  }
-}`);
+  const packageData = JSON.parse(fs.readFileSync(`packages/${dirent.name}/package.json`, 'utf8'));
+  const overridePackageData = {
+    name: `@damienmortini/${dirent.name}`,
+    description: `<${dirent.name}> custom element.`,
+    main: 'index.js',
+    scripts: {
+      test: 'echo "Error: no test specified" && exit 1',
+    },
+    keywords: [
+      'custom elements',
+      'web components',
+    ],
+    author: 'Damien Mortini',
+    license: 'ISC',
+    repository: {
+      type: 'git',
+      url: 'ssh://git@github.com/damienmortini/elements.git',
+      directory: `packages/${dirent.name}`,
+    },
+    bugs: {
+      url: 'https://github.com/damienmortini/elements/issues',
+    },
+    homepage: 'https://github.com/damienmortini/elements#readme',
+  };
+
+  fs.writeFileSync(`packages/${dirent.name}/package.json`, JSON.stringify({ ...packageData, ...overridePackageData }, null, '  '));
+
   fs.writeFileSync(`packages/${dirent.name}/README.md`, `# \`<${dirent.name}>\`
 
 ## Installation
