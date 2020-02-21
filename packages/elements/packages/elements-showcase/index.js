44ebc174407c3c49dd6c510947be443c883d5ee9
Damien Mortini <ddotmnn+git@gmail.com>
2020-02-21 16:09:43 +0100

    clean a bit showcase html

---
+++ packages/elements/packages/elements-showcase/index.js
@@ -0,0 +1,36 @@
+import ELEMENTS from './elements.js';
+
+const section = document.body.querySelector('section');
+const list = document.body.querySelector('nav ul');
+const iframe = document.body.querySelector('iframe');
+
+for (let elementName of ELEMENTS) {
+  let preview = '';
+  if (elementName instanceof Array) {
+    preview = elementName[1];
+    elementName = elementName[0];
+  }
+  const elementString = preview || `<${elementName}></${elementName}>`;
+  list.insertAdjacentHTML('beforeend', `
+        <li data-elementname="${elementName}">
+          <h2><a href="#${elementName}">${elementName}</a></h2>
+          <p>${elementString}</p>
+        </li>
+      `);
+  import(`./node_modules/@damienmortini/${elementName}/index.js`).then((module) => {
+    customElements.define(`${elementName}`, module.default);
+  });
+}
+
+const updatePath = () => {
+  const elementName = location.hash.slice(1);
+  if (elementName) {
+    section.classList.remove('grid');
+    iframe.contentWindow.location.replace(`${elementName}/index.html`);
+  } else {
+    section.classList.add('grid');
+  }
+};
+
+updatePath();
+window.addEventListener('hashchange', updatePath);
