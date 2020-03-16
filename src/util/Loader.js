91c6df449eb6e07c666d9ea580748c86de0160ac
Damien Mortini <ddotmnn+git@gmail.com>
2020-03-16 19:39:38 +0000

    small fix on type in Loader

--- src/util/Loader.js
+++ src/util/Loader.js
@@ -43,8 +43,10 @@ export class Loader {
       options.src = `${baseURI}${options.src}`;
 
       if (!options.type) {
-        const extension = /.*\.(.*)$/.exec(options.src)[1];
-        options.type = this.extensionTypeMap.get(extension);
+        const extensionResults = /.*\.(.*)$/.exec(options.src);
+        if (extensionResults) {
+          options.type = this.extensionTypeMap.get(extensionResults[1]);
+        }
       }
 
       promises.push(this._loadFile(options));
@@ -55,74 +57,74 @@ export class Loader {
 
   _loadFile({ src, type }) {
     return new Promise((resolve) => {
-      if (type.startsWith('image') || type.startsWith('video') || type.startsWith('audio')) {
+      if (type && (type.startsWith('image') || type.startsWith('video') || type.startsWith('audio'))) {
         resolve();
       } else {
         resolve(fetch(src));
       }
     })
-        .catch(() => {
-          return new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest;
-            xhr.onload = () => {
-              resolve(new Response(xhr.responseText, { status: xhr.status }));
-            };
-            xhr.open('GET', `${this.baseURI}${src}`);
-            xhr.send(null);
-          });
-        })
-        .then((response) => {
-          if (type === 'text/html') {
-            return response.text().then((html) => {
-              const template = document.createElement('template');
-              template.innerHTML = html;
-              return template;
-            });
-          } else if (type === 'text/css') {
-            return new Promise((resolve) => {
-              const link = document.createElement('link');
-              link.rel = 'stylesheet';
-              link.type = 'text/css';
-              const onLoad = () => {
-                link.removeEventListener('load', onLoad);
-                resolve(link);
-              };
-              link.addEventListener('load', onLoad);
-              link.href = src;
-              document.head.appendChild(link);
-            });
-          } else if (type.startsWith('text')) {
-            return response.text();
-          } else if (type === 'application/json') {
-            return response.json();
-          } else if (type.startsWith('image')) {
-            return new Promise((resolve) => {
-              const image = document.createElement('img');
-              image.onload = () => {
-                resolve(image);
-              };
-              image.src = src;
-            });
-          } else if (type.startsWith('video') || type.startsWith('audio')) {
-            return new Promise((resolve) => {
-              const media = document.createElement(type.split('/')[0]);
-              media.oncanplaythrough = () => {
-                resolve(media);
-              };
-              media.src = src;
-            });
-          } else if (type.startsWith('font')) {
-            return new Promise((resolve) => {
-              const fontFace = new FontFace(/([^\/]*)\.(woff|woff2|ttf)$/.exec(src)[1], `url("${src}")`);
-              document.fonts.add(fontFace);
-              return fontFace.load();
-            });
-          } else if (type === 'application/octet-stream') {
-            return response.arrayBuffer();
-          } else {
-            return response.blob();
-          }
+      .catch(() => {
+        return new Promise((resolve, reject) => {
+          const xhr = new XMLHttpRequest;
+          xhr.onload = () => {
+            resolve(new Response(xhr.responseText, { status: xhr.status }));
+          };
+          xhr.open('GET', `${this.baseURI}${src}`);
+          xhr.send(null);
         });
+      })
+      .then((response) => {
+        if (type === 'text/html') {
+          return response.text().then((html) => {
+            const template = document.createElement('template');
+            template.innerHTML = html;
+            return template;
+          });
+        } else if (type === 'text/css') {
+          return new Promise((resolve) => {
+            const link = document.createElement('link');
+            link.rel = 'stylesheet';
+            link.type = 'text/css';
+            const onLoad = () => {
+              link.removeEventListener('load', onLoad);
+              resolve(link);
+            };
+            link.addEventListener('load', onLoad);
+            link.href = src;
+            document.head.appendChild(link);
+          });
+        } else if (type.startsWith('text')) {
+          return response.text();
+        } else if (type === 'application/json') {
+          return response.json();
+        } else if (type.startsWith('image')) {
+          return new Promise((resolve) => {
+            const image = document.createElement('img');
+            image.onload = () => {
+              resolve(image);
+            };
+            image.src = src;
+          });
+        } else if (type.startsWith('video') || type.startsWith('audio')) {
+          return new Promise((resolve) => {
+            const media = document.createElement(type.split('/')[0]);
+            media.oncanplaythrough = () => {
+              resolve(media);
+            };
+            media.src = src;
+          });
+        } else if (type.startsWith('font')) {
+          return new Promise((resolve) => {
+            const fontFace = new FontFace(/([^\/]*)\.(woff|woff2|ttf)$/.exec(src)[1], `url("${src}")`);
+            document.fonts.add(fontFace);
+            return fontFace.load();
+          });
+        } else if (type === 'application/octet-stream') {
+          return response.arrayBuffer();
+        } else {
+          return response.blob();
+        }
+      });
   }
 }
 
