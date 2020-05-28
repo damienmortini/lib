a6ef7a7ef1932f6e326973303fc42ae4ede94bb5
Damien Mortini <ddotmnn+git@gmail.com>
2020-05-28 13:09:49 +0100

    revert copyExamples

--- packages/three/copyExamples.js
+++ packages/three/copyExamples.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
 const files = new Map([
-  ['../../three/examples/jsm/loaders/BasisTextureLoader.js', './loader/_BasisTextureLoader.js'],
-  ['../../three/examples/jsm/loaders/GLTFLoader.js', './loader/_GLTFLoader.js'],
-  ['../../three/examples/jsm/loaders/DRACOLoader.js', './loader/_DRACOLoader.js'],
-  ['../../three/examples/jsm/objects/Lensflare.js', './object/_Lensflare.js'],
+  [require.resolve('../../three/examples/jsm/loaders/BasisTextureLoader.js'), './loader/_BasisTextureLoader.js'],
+  [require.resolve('../../three/examples/jsm/loaders/GLTFLoader.js'), './loader/_GLTFLoader.js'],
+  [require.resolve('../../three/examples/jsm/loaders/DRACOLoader.js'), './loader/_DRACOLoader.js'],
+  [require.resolve('../../three/examples/jsm/objects/Lensflare.js'), './object/_Lensflare.js'],
 ]);
 for (const [source, destination] of files) {
   fs.copyFileSync(source, destination);
