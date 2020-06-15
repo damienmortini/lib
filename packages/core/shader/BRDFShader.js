ca123886d5a675bfedf982f4d201fb683314392d
Damien Mortini <ddotmnn+git@gmail.com>
2020-06-15 23:51:46 +0100

    add ggxSmith

--- packages/core/shader/BRDFShader.js
+++ packages/core/shader/BRDFShader.js
@@ -1,3 +1,5 @@
+// https://google.github.io/filament/Filament.md.html#materialsystem/diffusebrdf
+
 export default class BRDFShader {
   static ggx() {
     return `
@@ -12,6 +14,25 @@ export default class BRDFShader {
         vec3 h = normalize(-viewDirection - lightDirection);
         float NoH = clamp(dot(normal, h), 0.0, 1.0);
         return ggx(NoH, roughness);
+      }
+    `;
+  }
+
+
+  // TODO: fix
+  static ggxSmith() {
+    return `
+      float ggxSmith(float NoV, float NoL, float roughness) {
+        float a2 = roughness * roughness;
+        float GGXV = NoL * sqrt(NoV * NoV * (1.0 - a2) + a2);
+        float GGXL = NoV * sqrt(NoL * NoL * (1.0 - a2) + a2);
+        return 0.5 / (GGXV + GGXL);
+      }
+
+      float ggxSmith(vec3 viewDirection, vec3 lightDirection, vec3 normal, float roughness) {
+        float NoV = abs(dot(normal, -viewDirection)) + 1e-5;
+        float NoL = clamp(dot(normal, -lightDirection), 0.0, 1.0);
+        return ggxSmith(NoV, NoL, roughness);
       }
     `;
   }
