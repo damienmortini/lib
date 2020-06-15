0572c82ad17db6972e5db174a7c15b8c854e917d
Damien Mortini <ddotmnn+git@gmail.com>
2020-06-15 23:16:16 +0100

    THREE base shader for webgl2 by default

--- packages/three/shader/THREEBaseShader.js
+++ packages/three/shader/THREEBaseShader.js
@@ -1,12 +1,12 @@
 export default {
   vertexChunks: [
     ['start', `
-      varying vec3 vPosition;
-      varying vec3 vNormal;
-      varying vec2 vUV;
-      varying vec3 vWorldPosition;
-      varying vec3 vViewDirection;
-      varying float vFresnel;
+      out vec3 vPosition;
+      out vec3 vNormal;
+      out vec2 vUV;
+      out vec3 vWorldPosition;
+      out vec3 vViewDirection;
+      out float vFresnel;
     `],
     ['end', `
       vec3 worldPosition = (modelMatrix * vec4(position, 1.)).xyz;
@@ -22,12 +22,12 @@ export default {
   ],
   fragmentChunks: [
     ['start', `
-      varying vec3 vPosition;
-      varying vec3 vNormal;
-      varying vec2 vUV;
-      varying vec3 vWorldPosition;
-      varying vec3 vViewDirection;
-      varying float vFresnel;
+      in vec3 vPosition;
+      in vec3 vNormal;
+      in vec2 vUV;
+      in vec3 vWorldPosition;
+      in vec3 vViewDirection;
+      in float vFresnel;
     `],
   ],
 };
