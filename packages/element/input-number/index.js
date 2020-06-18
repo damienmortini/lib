adc89b82a93280c5f799f6b09d203fa86de2dce2
Damien Mortini <ddotmnn+git@gmail.com>
2020-06-18 12:46:54 +0100

    fix BeatSignalInputElement offsetX issue on chrome

--- packages/element/input-number/index.js
+++ packages/element/input-number/index.js
@@ -9,11 +9,11 @@ export default class NumberInputElement extends HTMLElement {
     this.attachShadow({ mode: 'open' }).innerHTML = `
       <style>
         :host {
-          display: inline-flex;
+          display: inline-block;
         }
         input {
-          flex: 1;
           width: 100%;
+          box-sizing: border-box;
         }
       </style>
       <input type="number">
