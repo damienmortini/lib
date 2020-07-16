573f111b4e3f3f31b5319cc34f95419a8b677990
Damien Mortini <ddotmnn+git@gmail.com>
2020-07-16 16:42:17 +0100

    add generator package

---
+++ packages/generator/README.md
@@ -0,0 +1,4 @@
+[![experimental](http://badges.github.io/stability-badges/dist/experimental.svg)](http://github.com/badges/stability-badges)
+
+# @damienmortini/generator
+Simple generator to create boilerplate files
---
+++ packages/generator/generators/package.js
@@ -0,0 +1,27 @@
+import fs from 'fs-extra';
+
+/**
+ * @param {String} name Name of the package
+ */
+export default function (name) {
+  fs.outputFileSync(`./packages/${name}/index.js`, `window.customElements.define('${name}', class extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' }).innerHTML = \`
+      <style>
+        :host {
+          display: block;
+        }
+      </style>
+      <slot></slot>
+    \`;
+  }
+});
+`);
+  fs.outputFileSync(`./packages/${name}/package.json`, JSON.stringify({
+    name: `@damienmortini/${name}`,
+    private: true,
+    version: '0.0.1',
+  }, null, 2));
+};
