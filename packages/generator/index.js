aaf767a45f154910bff6359cfd8b8d3b70d8414a
Damien Mortini <ddotmnn+git@gmail.com>
2020-07-16 16:42:17 +0100

    add generator package

---
+++ packages/generator/index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+import jsdoc2md from 'jsdoc-to-markdown';
+import readline from 'readline';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+(async function () {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const generatorName = process.argv[2];
+
+  const jsDocData = jsdoc2md.getTemplateDataSync({ files: `${__dirname}/generators/${generatorName}.js` });
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const params = [];
+  for (const param of jsDocData[0].params) {
+    params.push(await new Promise((resolve) => {
+      rl.question(`${param.description}: `, (answer) => {
+        resolve(answer);
+      });
+    }));
+  }
+
+  rl.close();
+
+  import(`./generators/${generatorName}.js`).then((value) => {
+    value.default(...params);
+  });
+})();
