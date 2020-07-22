e906a217cfd2e76ce84ec14bf94118efcfb249c4
Damien Mortini <ddotmnn+git@gmail.com>
2020-07-23 00:02:21 +0100

    add THREEBaseMaterial and fix skinning

---
+++ packages/three/material/THREEBaseMaterial.js
@@ -0,0 +1,63 @@
+import THREEShaderMaterial from './THREEShaderMaterial.js';
+import { ShaderChunk } from '../../../three/src/Three.js';
+
+export default class THREEBaseMaterial extends THREEShaderMaterial {
+  constructor({
+    vertexChunks = [],
+    fragmentChunks = [],
+    uniforms = {},
+    ...options
+  } = {}) {
+    super({
+      uniforms,
+      vertex: `
+        void main() {
+        }
+      `,
+      vertexChunks: [
+        ['start', `
+          ${options.skinning ? '#include <skinning_pars_vertex>' : ''}
+          
+          out vec3 vPosition;
+          out vec3 vNormal;
+          out vec2 vUV;
+          out vec3 vWorldPosition;
+          out vec3 vViewDirection;
+          out float vFresnel;
+        `],
+        ['main', `
+          vec3 transformedPosition = position;
+          vec3 transformedNormal = normal;
+          
+          ${options.skinning ? ShaderChunk.skinbase_vertex : ''}
+          ${options.skinning ? ShaderChunk.skinnormal_vertex.replace(/objectNormal/g, 'transformedNormal') : ''}
+          ${options.skinning ? ShaderChunk.skinning_vertex.replace(/transformed/g, 'transformedPosition') : ''}
+        `],
+        ['end', `
+          vec3 worldPosition = (modelMatrix * vec4(transformedPosition, 1.)).xyz;
+          gl_Position = projectionMatrix * viewMatrix * vec4(worldPosition, 1.);
+          
+          vWorldPosition = worldPosition;
+          vViewDirection = normalize(worldPosition - cameraPosition);
+          vPosition = transformedPosition;
+          vNormal = normalize(mat3(modelMatrix) * transformedNormal);
+          vUV = uv;
+          vFresnel = max(0., 1. - dot(-vViewDirection, vNormal));
+        `],
+        ...vertexChunks,
+      ],
+      fragmentChunks: [
+        ['start', `
+          in vec3 vPosition;
+          in vec3 vNormal;
+          in vec2 vUV;
+          in vec3 vWorldPosition;
+          in vec3 vViewDirection;
+          in float vFresnel;
+        `],
+        ...fragmentChunks,
+      ],
+      ...options,
+    });
+  }
+}
