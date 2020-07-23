68d5973257193526b40185bb8737200b6ee93d28
Damien Mortini <ddotmnn+git@gmail.com>
2020-07-23 17:47:05 +0100

    add threshold and smoothstep functions

--- packages/core/math/Math.js
+++ packages/core/math/Math.js
@@ -1,3 +1,12 @@
 export function modulo(x, y) {
   return ((x % y) + y) % y;
 }
+
+export function threshold(from, to, value) {
+  return Math.max(0, Math.min(1, (value - from) / (to - from)));
+}
+
+export function smoothstep(from, to, value) {
+  const t = threshold(from, to, value);
+  return t * t * (3.0 - 2.0 * t);
+}
