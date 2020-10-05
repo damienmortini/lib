75c0f2d47a5c99bbf6b01d51ad38ce573a739a05
Damien Mortini <ddotmnn+git@gmail.com>
2020-10-05 17:14:53 +0200

    refactor Animation

---
+++ packages/core/util/Animation.js
@@ -0,0 +1,41 @@
+import Ticker from './Ticker.js';
+
+const animate = (object, keyframes, { duration = 0, delay = 0, easing = (x) => x } = {}) => {
+  let finishedResolve;
+  const finished = new Promise((resolve) => finishedResolve = resolve);
+  let time = 0;
+  const keyframesMap = new Map(Object.entries(keyframes));
+  for (const [key, value] of keyframesMap) {
+    if (!(value instanceof Array)) {
+      keyframesMap.set(key, [object[key], value]);
+    }
+  }
+  const update = () => {
+    duration = duration || 1;
+    time += Ticker.deltaTime * 1000;
+
+    let progress;
+    if (time >= delay + duration) {
+      Ticker.delete(update);
+      progress = 1;
+    } else {
+      progress = Math.max(time - delay, 0) / duration;
+      progress = easing(progress);
+    }
+
+    for (const [key, value] of keyframesMap) {
+      object[key] = (value[1] - value[0]) * progress + value[0];
+    }
+
+    if (progress === 1) {
+      finishedResolve();
+    }
+  };
+  Ticker.add(update);
+  return {
+    finished,
+  };
+};
+
+
+export { animate };
