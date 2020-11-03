af66eaaa8f6d7eea342b8b71f27511fc9deaa731
Damien Mortini <ddotmnn+git@gmail.com>
2020-11-03 16:22:49 +0000

    small adds and fixes

--- packages/core/shader/BlurShader.js
+++ packages/core/shader/BlurShader.js
@@ -3,18 +3,18 @@
 export default class BlurShader {
   static blur() {
     return `
-      vec4 blur(sampler2D texture, vec2 uv, vec2 resolution, vec2 direction) {
+      vec4 blur(sampler2D textureToBlur, vec2 uv, vec2 resolution, vec2 direction) {
         vec4 color = vec4(0.0);
         vec2 off1 = vec2(1.411764705882353) * direction;
         vec2 off2 = vec2(3.2941176470588234) * direction;
         vec2 off3 = vec2(5.176470588235294) * direction;
-        color += texture2D(texture, uv) * 0.1964825501511404;
-        color += texture2D(texture, uv + (off1 / resolution)) * 0.2969069646728344;
-        color += texture2D(texture, uv - (off1 / resolution)) * 0.2969069646728344;
-        color += texture2D(texture, uv + (off2 / resolution)) * 0.09447039785044732;
-        color += texture2D(texture, uv - (off2 / resolution)) * 0.09447039785044732;
-        color += texture2D(texture, uv + (off3 / resolution)) * 0.010381362401148057;
-        color += texture2D(texture, uv - (off3 / resolution)) * 0.010381362401148057;
+        color += texture(textureToBlur, uv) * 0.1964825501511404;
+        color += texture(textureToBlur, uv + (off1 / resolution)) * 0.2969069646728344;
+        color += texture(textureToBlur, uv - (off1 / resolution)) * 0.2969069646728344;
+        color += texture(textureToBlur, uv + (off2 / resolution)) * 0.09447039785044732;
+        color += texture(textureToBlur, uv - (off2 / resolution)) * 0.09447039785044732;
+        color += texture(textureToBlur, uv + (off3 / resolution)) * 0.010381362401148057;
+        color += texture(textureToBlur, uv - (off3 / resolution)) * 0.010381362401148057;
         return color;
       }
     `;
