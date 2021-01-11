e5be3441e5ce8255e862b47db1d712a0c0988a5e
Damien Mortini <ddotmnn+git@gmail.com>
2021-01-11 21:09:06 +0100

    refactor node shader

--- packages/core/gl/GLTFNode.js
+++ packages/core/gl/GLTFNode.js
@@ -9,6 +9,63 @@ import GLBoxObject from './object/GLBoxObject.js';
 const QUATERNION = new Quaternion();
 const MATRIX4 = new Matrix4();
 
+const SKIN_SHADER = {
+  vertexChunks: [
+    ['start', `
+      uniform highp sampler2D jointMatricesTexture;
+      uniform vec2 jointMatricesTextureSize;
+
+      in uvec4 joint;
+      in vec4 weight;
+
+      flat out uvec4 vJoint;
+      out vec4 vWeight;
+
+      ${DataTextureShader.getTextureDataChunkFromUV()}
+      ${DataTextureShader.getTextureDataChunkFromIndex()}
+
+      mat4 getJointMatrix(int index, int matrixID) {                  
+        vec4 v1 = getTextureDataChunkFromIndex(jointMatricesTexture, index, 0 + matrixID * 4, 8, jointMatricesTextureSize);
+        vec4 v2 = getTextureDataChunkFromIndex(jointMatricesTexture, index, 1 + matrixID * 4, 8, jointMatricesTextureSize);
+        vec4 v3 = getTextureDataChunkFromIndex(jointMatricesTexture, index, 2 + matrixID * 4, 8, jointMatricesTextureSize);
+        vec4 v4 = getTextureDataChunkFromIndex(jointMatricesTexture, index, 3 + matrixID * 4, 8, jointMatricesTextureSize);
+        mat4 jointMatrix = mat4(v1, v2, v3, v4);
+        return jointMatrix;
+      }
+    `],
+    ['main', `
+      vec3 position = position;
+      vec3 normal = normal;
+
+      mat4 skinMatrix =
+        weight.x * getJointMatrix(int(joint.x), 0) +
+        weight.y * getJointMatrix(int(joint.y), 0) +
+        weight.z * getJointMatrix(int(joint.z), 0) +
+        weight.w * getJointMatrix(int(joint.w), 0);
+      position = (skinMatrix * vec4(position, 1.)).xyz;
+
+      mat4 skinNormalMatrix =
+        weight.x * getJointMatrix(int(joint.x), 1) +
+        weight.y * getJointMatrix(int(joint.y), 1) +
+        weight.z * getJointMatrix(int(joint.z), 1) +
+        weight.w * getJointMatrix(int(joint.w), 1);
+      normal = (skinNormalMatrix * vec4(normal, 1.)).xyz;
+
+      vJoint = joint;
+      vWeight = weight;
+    `],
+  ],
+  fragmentChunks: [
+    ['start', `
+      uniform highp sampler2D jointMatricesTexture;
+      uniform vec2 jointMatricesTextureSize;
+
+      flat in uvec4 vJoint;
+      in vec4 vWeight;
+    `],
+  ],
+};
+
 export default class GLTFNode {
   constructor({
     gl,
@@ -45,70 +102,12 @@ export default class GLTFNode {
             uniforms: {
               transform: this.transform,
             },
-            vertexChunks: [
-              ['start', `
-                uniform highp sampler2D jointMatricesTexture;
-                uniform vec2 jointMatricesTextureSize;
-
-                in uvec4 joint;
-                in vec4 weight;
-
-                flat out uvec4 vJoint;
-                out vec4 vWeight;
-
-                ${DataTextureShader.getTextureDataChunkFromUV()}
-                ${DataTextureShader.getTextureDataChunkFromIndex()}
-
-                mat4 getJointMatrix(int index, int matrixID) {                  
-                  vec4 v1 = getTextureDataChunkFromIndex(jointMatricesTexture, index, 0 + matrixID * 4, 8, jointMatricesTextureSize);
-                  vec4 v2 = getTextureDataChunkFromIndex(jointMatricesTexture, index, 1 + matrixID * 4, 8, jointMatricesTextureSize);
-                  vec4 v3 = getTextureDataChunkFromIndex(jointMatricesTexture, index, 2 + matrixID * 4, 8, jointMatricesTextureSize);
-                  vec4 v4 = getTextureDataChunkFromIndex(jointMatricesTexture, index, 3 + matrixID * 4, 8, jointMatricesTextureSize);
-                  mat4 jointMatrix = mat4(v1, v2, v3, v4);
-                  return jointMatrix;
-                }
-              `],
-              ['main', `
-                vec3 position = position;
-                vec3 normal = normal;
-
-                mat4 skinMatrix =
-                  weight.x * getJointMatrix(int(joint.x), 0) +
-                  weight.y * getJointMatrix(int(joint.y), 0) +
-                  weight.z * getJointMatrix(int(joint.z), 0) +
-                  weight.w * getJointMatrix(int(joint.w), 0);
-                position = (skinMatrix * vec4(position, 1.)).xyz;
-
-                mat4 skinNormalMatrix =
-                  weight.x * getJointMatrix(int(joint.x), 1) +
-                  weight.y * getJointMatrix(int(joint.y), 1) +
-                  weight.z * getJointMatrix(int(joint.z), 1) +
-                  weight.w * getJointMatrix(int(joint.w), 1);
-                normal = (skinNormalMatrix * vec4(normal, 1.)).xyz;
-
-                vJoint = joint;
-                vWeight = weight;
-            `],
-            ],
+            vertexChunks: this.skin ? SKIN_SHADER.vertexChunks : [],
             fragmentChunks: [
-              ['start', `
-                uniform highp sampler2D jointMatricesTexture;
-                uniform vec2 jointMatricesTextureSize;
-
-                flat in uvec4 vJoint;
-                in vec4 vWeight;
-              `],
               ['end', `
                 fragColor = vec4(vNormal * .5 + .5, 1.);
-                // fragColor = vec4(vUV, 0., 1.);
-                // fragColor = .5 + texture(jointMatricesTexture, vUV);
-                // fragColor = vec4(jointMatricesTextureSize, 0., 1.);
-                // fragColor = vWeight;
-                // fragColor = vec4(vJoint);
-                // fragColor = vec4(vec3(vJoint.y), 1.);
-                // fragColor = vec4(vec3(vJoint.y), 1.);
-                // fragColor = vec4(vec3(vWeight.y), 1.);
               `],
+              ...(this.skin ? SKIN_SHADER.fragmentChunks : []),
             ],
           }),
         }),
