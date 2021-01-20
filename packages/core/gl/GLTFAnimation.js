60c4c723742d96bace2d3d6e03be6519000117e9
Damien Mortini <ddotmnn+git@gmail.com>
2021-01-20 15:14:44 +0100

    GLTFAnimation: add CUBICSPLINE interpolation

--- packages/core/gl/GLTFAnimation.js
+++ packages/core/gl/GLTFAnimation.js
@@ -1,5 +1,3 @@
-import { threshold } from '../math/Math.js';
-
 const NODES_NEEDING_MATRIX_UPDATE = new Set();
 
 export default class GLTFAnimation {
@@ -44,24 +42,42 @@ export default class GLTFAnimation {
         nextIndex++;
       }
       const previousIndex = !nextIndex ? 0 : nextIndex - 1;
-      const alpha = threshold(inputArray[previousIndex], inputArray[nextIndex], this._currentTime);
-      if (channel.target.path === 'translation' || channel.target.path === 'scale') {
-        const previousValue = outputArray.subarray(previousIndex * 3, previousIndex * 3 + 3);
-        const nextValue = outputArray.subarray(nextIndex * 3, nextIndex * 3 + 3);
-        node[channel.target.path].copy(previousValue).lerp(nextValue, alpha);
-        NODES_NEEDING_MATRIX_UPDATE.add(node);
-      } else if (channel.target.path === 'rotation') {
-        const previousValue = outputArray.subarray(previousIndex * 4, previousIndex * 4 + 4);
-        const nextValue = outputArray.subarray(nextIndex * 4, nextIndex * 4 + 4);
-        node.rotation.copy(previousValue).slerp(nextValue, alpha);
-        NODES_NEEDING_MATRIX_UPDATE.add(node);
-      } else if (channel.target.path === 'weights') {
-        const length = node.weights.length;
-        const previousValue = outputArray.subarray(previousIndex * length, previousIndex * length + length);
-        const nextValue = outputArray.subarray(nextIndex * length, nextIndex * length + length);
-        for (let index = 0; index < node.weights.length; index++) {
-          node.weights[index] = previousValue[index] + (nextValue[index] - previousValue[index]) * alpha;
+      const keyDelta = inputArray[nextIndex] - inputArray[previousIndex];
+      const alpha = (this._currentTime - inputArray[previousIndex]) / keyDelta;
+      const targetArray = node[channel.target.path];
+      const length = targetArray.length;
+
+      if (channel.sampler.interpolation === 'CUBICSPLINE') {
+        const previousIndexComputed = previousIndex * length * 3;
+        const nextIndexComputed = nextIndex * length * 3;
+
+        const tSquare = alpha ** 2;
+        const tCubic = alpha ** 3;
+
+        for (let index = 0; index < length; index++) {
+          const v0 = outputArray[previousIndexComputed + index + length];
+          const a = keyDelta * outputArray[nextIndexComputed + index];
+          const b = keyDelta * outputArray[previousIndexComputed + index + 2 * length];
+          const v1 = outputArray[nextIndexComputed + index + length];
+
+          targetArray[index] = ((2 * tCubic - 3 * tSquare + 1) * v0) + ((tCubic - 2 * tSquare + alpha) * b) + ((-2 * tCubic + 3 * tSquare) * v1) + ((tCubic - tSquare) * a);
         }
+      } else {
+        if (channel.target.path === 'rotation') {
+          const previousValue = outputArray.subarray(previousIndex * 4, previousIndex * 4 + 4);
+          const nextValue = outputArray.subarray(nextIndex * 4, nextIndex * 4 + 4);
+          node.rotation.copy(previousValue).slerp(nextValue, alpha);
+        } else {
+          const previousValue = outputArray.subarray(previousIndex * length, previousIndex * length + length);
+          const nextValue = outputArray.subarray(nextIndex * length, nextIndex * length + length);
+          for (let index = 0; index < length; index++) {
+            targetArray[index] = previousValue[index] + (nextValue[index] - previousValue[index]) * alpha;
+          }
+        }
+      }
+
+      if (channel.target.path === 'translation' || channel.target.path === 'scale' || channel.target.path === 'rotation') {
+        NODES_NEEDING_MATRIX_UPDATE.add(node);
       }
     }
 
