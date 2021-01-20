2f8d98bf521786a73560c8e1bd381a1979823176
Damien Mortini <ddotmnn+git@gmail.com>
2021-01-20 15:15:02 +0100

    MappingShader: fix UVs

--- packages/core/shader/MappingShader.js
+++ packages/core/shader/MappingShader.js
@@ -2,8 +2,8 @@ export default class MappingShader {
   static directionToEquirectangularUV() {
     return `
       vec2 directionToEquirectangularUV(vec3 direction) {
-        float u = atan(-direction.z, direction.x) * 0.15915494309189535 + .5;
-        float v = asin(clamp(direction.y, -1., 1.)) * 0.3183098861837907 + .5;
+        float u = atan(direction.x, direction.z) * 0.15915494309189535 + .5;
+        float v = asin(clamp(-direction.y, -1., 1.)) * 0.3183098861837907 + .5;
         return vec2(u, v);
       }
     `;
