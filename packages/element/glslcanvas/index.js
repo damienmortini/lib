16068c45a67d545d317128cd74dd9e06b7dc44ba
Damien Mortini <ddotmnn+git@gmail.com>
2021-02-01 17:05:42 +0100

    gl-view > glslcanvas

---
+++ packages/element/glslcanvas/index.js
@@ -0,0 +1,106 @@
+import GLPlaneObject from '../core/gl/object/GLPlaneObject.js';
+import GLProgram from '../core/gl/GLProgram.js';
+import Shader from '../core/3d/Shader.js';
+
+export default class GLSLCanvasElement extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' }).innerHTML = `
+      <style>
+        :host {
+          display: block;
+          touch-action: none;
+        }
+        
+        canvas {
+          width: 100%;
+          height: 100%;
+          max-height: 100%;
+        }
+      </style>
+      <canvas></canvas>
+    `;
+
+    this.canvas = this.shadowRoot.querySelector('canvas');
+
+    const webGLOptions = {
+      antialias: true,
+      premultipliedAlpha: false,
+    };
+
+    this.gl = this.canvas.getContext('webgl2', webGLOptions);
+    if (!this.gl) {
+      this.gl = this.canvas.getContext('webgl', webGLOptions) || this.canvas.getContext('experimental-webgl', webGLOptions);
+    }
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      const width = entries[0].contentRect.width;
+      const height = entries[0].contentRect.height;
+
+      this.canvas.width = width * window.devicePixelRatio;
+      this.canvas.height = height * window.devicePixelRatio;
+
+      this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+      this.object.program.uniforms.set('glslCanvasSize', [this.canvas.width, this.canvas.height]);
+      this.draw();
+    });
+    resizeObserver.observe(this.canvas);
+
+    this.object = new GLPlaneObject({
+      gl: this.gl,
+      width: 2,
+      height: 2,
+    });
+    this.object.bind();
+  }
+
+  get shader() {
+    return this._shader;
+  }
+
+  set shader(value) {
+    this._shader = value;
+    this.object.program = new GLProgram({
+      gl: this.gl,
+      shader: new Shader({
+        uniforms: {
+          glslCanvasSize: [this.canvas.width, this.canvas.height],
+          ...this._shader.uniforms,
+        },
+        vertexChunks: [
+          ['end', `
+            vPosition = position.xy;
+            gl_Position = vec4(position, 1.);
+          `],
+          ...this._shader.vertexChunks ?? [],
+          ['start', `
+            uniform vec2 glslCanvasSize;
+            in vec3 position;
+            out vec2 vPosition;
+          `],
+        ],
+        fragmentChunks: [
+          ...this._shader.fragmentChunks ?? [],
+          ['start', `
+            uniform vec2 glslCanvasSize;
+            in vec2 vPosition;
+          `],
+        ],
+      }),
+    });
+    this.object.bind();
+    this.draw();
+  }
+
+  get uniforms() {
+    return this.object.program.uniforms;
+  }
+
+  draw() {
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+    this.object.draw();
+  }
+}
+
+customElements.define('damo-glslcanvas', GLSLCanvasElement);
