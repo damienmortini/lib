fe1a2995a2f63aa274eb8fb8119d604601904d5f
Damien Mortini <ddotmnn+git@gmail.com>
2021-02-09 11:43:45 +0100

    generator: get defaults from package.json

--- packages/generator/index.js
+++ packages/generator/index.js
@@ -2,6 +2,7 @@
 
 import jsdoc2md from 'jsdoc-to-markdown';
 import readline from 'readline';
+import fs from 'fs-extra';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
@@ -18,17 +19,22 @@ import { dirname } from 'path';
     output: process.stdout,
   });
 
+  const rootPackage = fs.readJSONSync(`./package.json`);
+  const rootPackageNameSplitted = rootPackage.name.split('/');
   const options = {
-    scope: /(?!.*@)(.*?)[\\/]/.exec(process.cwd())[1],
+    name: rootPackageNameSplitted[1] ?? rootPackageNameSplitted[0],
   };
+  if (rootPackageNameSplitted.length === 2) options.scope = rootPackageNameSplitted[0].slice(1);
+
   for (const parameter of jsDocData[0].params) {
     if (!parameter.description) continue;
     const name = parameter.name.split('.')[1];
     const defaultValue = options[name] ?? parameter.defaultvalue;
     options[name] = await new Promise((resolve) => {
-      rl.question(`${parameter.description}${defaultValue ? ' [' + (defaultValue) + ']' : ''}: `, (answer) => {
-        resolve(answer || defaultValue);
+      rl.question(`${parameter.description}: `, (answer) => {
+        resolve(answer);
       });
+      if (defaultValue) rl.write(defaultValue);
     });
   }
 
