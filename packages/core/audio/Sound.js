bae8a8563ef7d8c06e314a5d829831ed8a65f654
Damien Mortini <ddotmnn+git@gmail.com>
2021-03-17 18:29:59 +0100

    refactor Sound.js to use only AudioContext

--- packages/core/audio/Sound.js
+++ packages/core/audio/Sound.js
@@ -1,6 +1,14 @@
+import Ticker from '../../core/util/Ticker.js';
+
 let baseURI = '';
 
-export default class Sound extends Audio {
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+window.addEventListener('pointerup', () => {
+  audioContext.resume();
+});
+
+export default class Sound {
   static get baseURI() {
     return baseURI;
   }
@@ -10,32 +18,89 @@ export default class Sound extends Audio {
   }
 
   constructor({ src }) {
-    if (src instanceof Array) {
-      super();
-      for (const url of src) {
-        const source = document.createElement('source');
-        source.src = `${Sound.baseURI}${url}`;
-        this.appendChild(source);
-      }
-    } else {
-      super(`${Sound.baseURI}${src}`);
-    }
+    this.paused = true;
+    this.allowMultiplePlayback = false;
 
-    const load = () => {
-      const paused = this.paused;
-      this.play().then(() => {
-        if (paused && !this.autoplay) {
-          this.pause();
-        }
-      });
-    };
+    this._startTime = 0;
+    this._currentTime = 0;
 
-    window.addEventListener('pointerup', load, { once: true });
+    this._gainNode = audioContext.createGain();
+    this._gainNode.connect(audioContext.destination);
+
+    this._updateBound = this._update.bind(this);
+
+    this.ready = this._load({ src: `${Sound.baseURI}${src}` });
+  }
+
+  async _load({ src }) {
+    const audioArrayBuffer = await fetch(src).then((response) => response.arrayBuffer());
+    const audioBuffer = await new Promise((resolve) => audioContext.decodeAudioData(audioArrayBuffer, (data) => resolve(data)));
+    this._buffer = audioBuffer;
+    if (!this.paused || this.autoplay) this.play();
   }
 
   play() {
-    return super.play().catch((error) => {
-      console.warn(error);
-    });
+    this.paused = false;
+    if (!this._buffer) return;
+    if (!this.allowMultiplePlayback || this._startTime === audioContext.currentTime) {
+      this._source?.stop();
+    }
+    this._source = audioContext.createBufferSource();
+    this._source.buffer = this._buffer;
+    this._source.connect(this._gainNode);
+    this._source.loop = this.loop;
+    this._startTime = audioContext.currentTime;
+    this._source.start(0, this._currentTime);
+    Ticker.add(this._updateBound);
+  }
+
+  pause() {
+    Ticker.delete(this._updateBound);
+    this.paused = true;
+    this._source?.stop();
+  }
+
+  get duration() {
+    return this._buffer?.duration ?? 0;
+  }
+
+  get autoplay() {
+    return this._autoplay;
+  }
+
+  set autoplay(value) {
+    if (this._autoplay === value) return;
+    this._autoplay = value;
+    if (this._autoplay) this.play();
+  }
+
+  get currentTime() {
+    return this._currentTime;
+  }
+
+  set currentTime(value) {
+    this._currentTime = value;
+    if (!this.paused) this.play();
+  }
+
+  get loop() {
+    return this._loop;
+  }
+
+  set loop(value) {
+    this._loop = value;
+    if (this._source) this._source.loop = this._loop;
+  }
+
+  get volume() {
+    return this._gainNode.gain.value;
+  }
+
+  set volume(value) {
+    this._gainNode.gain.value = value;
+  }
+
+  _update() {
+    this._currentTime = (audioContext.currentTime - this._startTime) % this.duration;
   }
 }
