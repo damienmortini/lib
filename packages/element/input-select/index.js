28f2d8c7e9eaa83c4b6400fc09d908b48a8db38b
Damien Mortini <ddotmnn+git@gmail.com>
2021-06-22 17:05:53 -0600

    element-input-select: improve and optimize

--- packages/element/input-select/index.js
+++ packages/element/input-select/index.js
@@ -18,13 +18,14 @@ export default class InputSelectElement extends HTMLElement {
       <select></select>
     `;
 
-    this._optionsMap = new Map();
-
     this._select = this.shadowRoot.querySelector('select');
-    this._select.addEventListener('change', () => this.value = this._select.value);
+    this._select.addEventListener('change', (event) => {
+      event.stopPropagation();
+      this.value = this._options[this._select.selectedIndex];
+    });
     this._select.addEventListener('input', (event) => {
       event.stopPropagation();
-      this.value = this._select.value;
+      this.value = this._options[this._select.selectedIndex];
       this.dispatchEvent(new Event('input', { bubbles: true }));
     });
   }
@@ -66,28 +67,24 @@ export default class InputSelectElement extends HTMLElement {
   set options(value) {
     this._options = value;
     this._select.innerHTML = '';
-    this._optionsMap.clear();
-    for (const option of this._options) {
+    for (const [index, option] of this._options.entries()) {
       const optionElement = document.createElement('option');
-      const stringifiedOption = typeof option === 'object' ? JSON.stringify(option) : option.toString();
-      optionElement.value = stringifiedOption;
-      optionElement.text = stringifiedOption;
+      optionElement.value = index;
+      optionElement.text = JSON.stringify(option);
       optionElement.selected = option === this.value;
       this._select.add(optionElement);
-      this._optionsMap.set(stringifiedOption, option);
     }
-    this.value = this._value;
   }
 
   get value() {
-    return this._optionsMap.get(this._select.value);
+    return this._value;
   }
 
   set value(value) {
     if (this._value === value) {
       return;
     }
-    this._select.value = value;
+    this._select.selectedIndex = this._options.indexOf(value);
     this._value = value;
     this.dispatchEvent(new Event('change', { bubbles: true }));
   }
