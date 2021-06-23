716eaebac27c1a94fc64f5aec46135dec641774c
Damien Mortini <ddotmnn+git@gmail.com>
2021-06-23 10:17:34 -0600

    update gui server

--- packages/gui-server/index.js
+++ packages/gui-server/index.js
@@ -1,28 +1,9 @@
-const guiServers = new Map();
-
 export default class GUIServer {
-  static get(value) {
-    let guiServer = guiServers.get(value);
-    if (!guiServer) guiServer = new GUIServer({ name: value });
-    return guiServer;
-  }
-
-  constructor({ name = undefined } = {}) {
-    if (name) guiServers.set(name, this);
-    this._guiElements = new Set();
-  }
-
-  connect(guiElement) {
-    this._guiElements.add(guiElement);
-  }
-
-  disconnect(guiElement) {
-    this._guiElements.delete(guiElement);
-  }
-
-  add(options) {
-    for (const guiElement of this._guiElements) {
-      guiElement.add(options);
-    }
+  static add(options) {
+    window.dispatchEvent(new CustomEvent('damoguiadd', {
+      detail: {
+        options,
+      },
+    }));
   }
 }
