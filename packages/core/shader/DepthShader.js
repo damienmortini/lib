23495af1b786f77197a792ab91d86d10bb9f1448
Damien Mortini <ddotmnn+git@gmail.com>
2021-10-08 18:21:29 -0400

    fix exports

--- packages/core/shader/DepthShader.js
+++ packages/core/shader/DepthShader.js
@@ -1,6 +1,5 @@
-export default class DepthShader {
-  static pack() {
-    return `
+export const pack = () => {
+  return `
       vec4 pack (float depth) {
         const vec4 bitSh = vec4(256 * 256 * 256, 256 * 256, 256, 1.0);
         const vec4 bitMsk = vec4(0, 1.0 / 256.0, 1.0 / 256.0, 1.0 / 256.0);
@@ -9,25 +8,21 @@ export default class DepthShader {
         return comp;
       }
     `
-  }
+}
 
-  static unpack() {
-    return `
+export const unpack = () => {
+  return `
       float unpack (vec4 packedDepth) {
         const vec4 bitShifts = vec4(1.0 / (256.0 * 256.0 * 256.0), 1.0 / (256.0 * 256.0), 1.0 / 256.0, 1);
         return dot(packedDepth, bitShifts);
       }
     `
-  }
+}
 
-  static bumpFromDepth({
-    getDepth = 'return 0.;',
-  } = {}) {
-    return `
-      float getDepth(vec2 position) {
-        ${getDepth}
-      }
-
+export const bumpFromDepth = ({
+  getDepthFunctionName = 'getDepth',
+} = {}) => {
+  return `
       vec4 bumpFromDepth(sampler2D depthTexture, vec2 uv, vec2 resolution, float scale) {
         vec2 step = 1. / resolution;
           
@@ -44,15 +39,14 @@ export default class DepthShader {
       vec4 bumpFromDepth(vec2 uv, vec2 resolution, float scale) {
         vec2 step = 1. / resolution;
           
-        float depth = getDepth(uv);
+        float depth = ${getDepthFunctionName}(uv);
           
         vec2 dxy = depth - vec2(
-            getDepth(uv + vec2(step.x, 0.)), 
-            getDepth(uv + vec2(0., step.y))
+            ${getDepthFunctionName}(uv + vec2(step.x, 0.)), 
+            ${getDepthFunctionName}(uv + vec2(0., step.y))
         );
           
         return vec4(depth, normalize(vec3(dxy * scale / step, 1.)));
       }
     `
-  }
 }
