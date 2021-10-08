2761e16d83e8a4a5ff43b3cb3f85ab70a19f3a9c
Damien Mortini <ddotmnn+git@gmail.com>
2021-10-08 18:21:29 -0400

    fix exports

--- packages/core/math/Color.js
+++ packages/core/math/Color.js
@@ -1,69 +1,70 @@
-const element = document.createElement('br')
-element.style.display = 'none'
-document.body.appendChild(element)
+let element
 
-export default class Color {
-  static styleToRGBA(value) {
-    element.style.color = value
-
-    const results = /rgba?\s*\(\s*(\d*),\s*(\d*)\s*,\s*(\d*)\s*(,\s*([.\d]*))?\s*\)/.exec(getComputedStyle(element).getPropertyValue('color'))
-    return results ? [
-      parseInt(results[1]) / 255,
-      parseInt(results[2]) / 255,
-      parseInt(results[3]) / 255,
-      results[5] !== undefined ? parseFloat(results[5]) : 1,
-    ] : null
+export const styleToRGBA = (value) => {
+  if (!element) {
+    element = document.createElement('br')
+    element.style.display = 'none'
+    document.body.appendChild(element)
   }
+  element.style.color = value
 
-  static RGBToHSL(rgb, output = []) {
-    const r = rgb[0]
-    const g = rgb[1]
-    const b = rgb[2]
+  const results = /rgba?\s*\(\s*(\d*),\s*(\d*)\s*,\s*(\d*)\s*(,\s*([.\d]*))?\s*\)/.exec(getComputedStyle(element).getPropertyValue('color'))
+  return results ? [
+    parseInt(results[1]) / 255,
+    parseInt(results[2]) / 255,
+    parseInt(results[3]) / 255,
+    results[5] !== undefined ? parseFloat(results[5]) : 1,
+  ] : null
+}
 
-    const max = Math.max(r, g, b)
-    const min = Math.min(r, g, b)
+export const RGBToHSL = (rgb, output = []) => {
+  const r = rgb[0]
+  const g = rgb[1]
+  const b = rgb[2]
 
-    let hue
-    let saturation
+  const max = Math.max(r, g, b)
+  const min = Math.min(r, g, b)
 
-    const lightness = (min + max) / 2.0
+  let hue
+  let saturation
 
-    if (min === max) {
-      hue = 0
-      saturation = 0
-    } else {
-      const delta = max - min
-      saturation = lightness <= 0.5 ? delta / (max + min) : delta / (2 - max - min)
-      switch (max) {
-        case r: hue = (g - b) / delta + (g < b ? 6 : 0); break
-        case g: hue = (b - r) / delta + 2; break
-        case b: hue = (r - g) / delta + 4; break
-      }
-      hue /= 6
+  const lightness = (min + max) / 2.0
+
+  if (min === max) {
+    hue = 0
+    saturation = 0
+  } else {
+    const delta = max - min
+    saturation = lightness <= 0.5 ? delta / (max + min) : delta / (2 - max - min)
+    switch (max) {
+      case r: hue = (g - b) / delta + (g < b ? 6 : 0); break
+      case g: hue = (b - r) / delta + 2; break
+      case b: hue = (r - g) / delta + 4; break
     }
-
-    output[0] = hue
-    output[1] = saturation
-    output[2] = lightness
-
-    return output
+    hue /= 6
   }
 
-  static hexToRGB(hex) {
-    const results = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-    return results ? [
-      parseInt(results[1], 16) / 255,
-      parseInt(results[2], 16) / 255,
-      parseInt(results[3], 16) / 255,
-    ] : null
-  }
+  output[0] = hue
+  output[1] = saturation
+  output[2] = lightness
 
-  static rgbToHex(rgb) {
-    let hex = '#'
-    for (const component of rgb) {
-      const componentString = Math.floor(component * 255).toString(16)
-      hex += componentString.length == 1 ? '0' + componentString : componentString
-    }
-    return hex
+  return output
+}
+
+export const hexToRGB = (hex) => {
+  const results = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+  return results ? [
+    parseInt(results[1], 16) / 255,
+    parseInt(results[2], 16) / 255,
+    parseInt(results[3], 16) / 255,
+  ] : null
+}
+
+export const rgbToHex = (rgb) => {
+  let hex = '#'
+  for (const component of rgb) {
+    const componentString = Math.floor(component * 255).toString(16)
+    hex += componentString.length == 1 ? '0' + componentString : componentString
   }
+  return hex
 }
