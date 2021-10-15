e5e6b9660ab144d85741080ceb296b290db06ac6
Damien Mortini <ddotmnn+git@gmail.com>
2021-10-15 18:12:17 -0400

    three: small updates

--- packages/core/shader/BlurShader.js
+++ packages/core/shader/BlurShader.js
@@ -1,22 +1,20 @@
 // From https://github.com/Jam3/glsl-fast-gaussian-blur
 
-export default class BlurShader {
-  static blur() {
-    return `
-      vec4 blur(sampler2D textureToBlur, vec2 uv, vec2 resolution, vec2 direction) {
-        vec4 color = vec4(0.0);
-        vec2 off1 = vec2(1.411764705882353) * direction;
-        vec2 off2 = vec2(3.2941176470588234) * direction;
-        vec2 off3 = vec2(5.176470588235294) * direction;
-        color += texture(textureToBlur, uv) * 0.1964825501511404;
-        color += texture(textureToBlur, uv + (off1 / resolution)) * 0.2969069646728344;
-        color += texture(textureToBlur, uv - (off1 / resolution)) * 0.2969069646728344;
-        color += texture(textureToBlur, uv + (off2 / resolution)) * 0.09447039785044732;
-        color += texture(textureToBlur, uv - (off2 / resolution)) * 0.09447039785044732;
-        color += texture(textureToBlur, uv + (off3 / resolution)) * 0.010381362401148057;
-        color += texture(textureToBlur, uv - (off3 / resolution)) * 0.010381362401148057;
-        return color;
-      }
-    `
-  }
+export const blur = () => {
+  return `
+    vec4 blur(sampler2D textureToBlur, vec2 uv, vec2 resolution, vec2 direction, float lod) {
+      vec4 color = vec4(0.0);
+      vec2 off1 = vec2(1.411764705882353) * direction;
+      vec2 off2 = vec2(3.2941176470588234) * direction;
+      vec2 off3 = vec2(5.176470588235294) * direction;
+      color += textureLod(textureToBlur, uv, lod) * 0.1964825501511404;
+      color += textureLod(textureToBlur, uv + (off1 / resolution), lod) * 0.2969069646728344;
+      color += textureLod(textureToBlur, uv - (off1 / resolution), lod) * 0.2969069646728344;
+      color += textureLod(textureToBlur, uv + (off2 / resolution), lod) * 0.09447039785044732;
+      color += textureLod(textureToBlur, uv - (off2 / resolution), lod) * 0.09447039785044732;
+      color += textureLod(textureToBlur, uv + (off3 / resolution), lod) * 0.010381362401148057;
+      color += textureLod(textureToBlur, uv - (off3 / resolution), lod) * 0.010381362401148057;
+      return color;
+    }
+  `
 }
