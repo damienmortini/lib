1fa4e3d9f0f86dfabfc0ffb0baddca1622236031
Damien Mortini <ddotmnn+git@gmail.com>
2021-11-02 16:44:18 -0400

    MathShader: update export

--- packages/core/shader/MathShader.js
+++ packages/core/shader/MathShader.js
@@ -1,9 +1,7 @@
-export default class MathShader {
-  static threshold() {
-    return `
-      float threshold(float edge0, float edge1, float x) {
-        return clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0);
-      }
-    `
-  }
+export const threshold = () => {
+  return `
+    float threshold(float edge0, float edge1, float x) {
+      return clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0);
+    }
+  `
 }
