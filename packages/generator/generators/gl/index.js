10ec2ab96858b1964061cc893a0612df5816a8a9
Damien Mortini <ddotmnn+git@gmail.com>
2021-11-28 22:02:43 -0500

    generator: update gl

--- packages/generator/generators/gl/index.js
+++ packages/generator/generators/gl/index.js
@@ -22,13 +22,15 @@ export default function ({ elementScope, elementName, scope = 'damienmortini', p
     dependencies: {
       '@damienmortini/core': '*',
       '@damienmortini/damdom-ticker': '*',
+      '@damienmortini/math': '*',
+      '@damienmortini/webgl': '*',
     },
   }, null, 2))
-  let indexFileContent = fs.readFileSync(`${__dirname}/template.js`, { encoding: 'utf-8' })
+  let indexFileContent = fs.readFileSync(`${__dirname}/template/index.js`, { encoding: 'utf-8' })
   indexFileContent = indexFileContent.replaceAll('template-element', fullElementName)
   indexFileContent = indexFileContent.replaceAll('template title', elementName)
   const elementClass = `${elementScope} ${elementName}`.replace(/(^| )(.)/g, (result) => result.replace(' ', '').toUpperCase()) + 'Element'
   indexFileContent = indexFileContent.replaceAll('TemplateElement', elementClass)
   fs.writeFileSync(`${fullPath}/index.js`, indexFileContent)
-  fs.copyFileSync(`${__dirname}/View.js`, `${fullPath}/View.js`)
+  fs.copyFileSync(`${__dirname}/template/View.js`, `${fullPath}/View.js`)
 }
