0fb11ef9d5fcbea585578e0d245e69708a266d38
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2022-02-17 16:39:39 +0100

    DamdomPadXYElement: add min/max

--- packages/damdom/damdom-padxy/index.js
+++ packages/damdom/damdom-padxy/index.js
@@ -1,12 +1,18 @@
 export default class DamdomPadXYElement extends HTMLElement {
+  #value = [0, 0]
+  #pad
+  #pointer
+  #width = 100
+  #height = 100
+  #min = [-1, -1]
+  #max = [1, 1]
+
   static get observedAttributes() {
-    return ['value', 'disabled']
+    return ['value', 'disabled', 'min', 'max']
   }
 
   constructor() {
     super()
-
-    this._value = [0, 0]
 
     this.attachShadow({ mode: 'open' }).innerHTML = `
       <style>
@@ -25,7 +31,7 @@ export default class DamdomPadXYElement extends HTMLElement {
           opacity: .5;
         }
 
-        .pad {
+        #pad {
           position: absolute;
           top: 0;
           left: 0;
@@ -37,7 +43,7 @@ export default class DamdomPadXYElement extends HTMLElement {
           background-position: -.5px -.5px;
           touch-action: none;
         }
-        .pointer {
+        #pointer {
           position: absolute;
           top: 50%;
           left: 50%;
@@ -51,18 +57,18 @@ export default class DamdomPadXYElement extends HTMLElement {
           pointer-events: none;
         }
       </style>
-      <div class="pad"></div>
-      <div class="pointer"></div>
+      <div id="pad"></div>
+      <div id="pointer"></div>
     `
 
-    this._pad = this.shadowRoot.querySelector('.pad')
-    this._pointer = this.shadowRoot.querySelector('.pointer')
+    this.#pad = this.shadowRoot.querySelector('#pad')
+    this.#pointer = this.shadowRoot.querySelector('#pointer')
 
     const resizeObserver = new ResizeObserver((entries) => {
-      this._width = entries[0].contentRect.width
-      this._height = entries[0].contentRect.height
+      this.#width = entries[0].contentRect.width
+      this.#height = entries[0].contentRect.height
 
-      this._updatePointer()
+      this.#updatePointer()
     })
     resizeObserver.observe(this)
 
@@ -71,11 +77,11 @@ export default class DamdomPadXYElement extends HTMLElement {
 
     const updatePointer = (event) => {
       event.preventDefault()
-      let x = ((pointerDownPosition[0] + event.screenX - pointerDownScreenPosition[0]) / this._pad.offsetWidth) * 2 - 1
-      let y = -(((pointerDownPosition[1] + event.screenY - pointerDownScreenPosition[1]) / this._pad.offsetHeight) * 2 - 1)
-      x = Math.max(Math.min(1, x), -1)
-      y = Math.max(Math.min(1, y), -1)
-      this.value = [x, y]
+      let x = ((pointerDownPosition[0] + event.screenX - pointerDownScreenPosition[0]) / this.#pad.offsetWidth)
+      let y = 1 - ((pointerDownPosition[1] + event.screenY - pointerDownScreenPosition[1]) / this.#pad.offsetHeight)
+      x = Math.max(Math.min(1, x), 0)
+      y = Math.max(Math.min(1, y), 0)
+      this.value = [this.#min[0] + x * (this.#max[0] - this.#min[0]), this.#min[1] + y * (this.#max[1] - this.#min[1])]
     }
 
     const onPointerUp = (event) => {
@@ -84,7 +90,7 @@ export default class DamdomPadXYElement extends HTMLElement {
       window.removeEventListener('pointermove', updatePointer)
     }
 
-    this._pad.addEventListener('pointerdown', (event) => {
+    this.#pad.addEventListener('pointerdown', (event) => {
       pointerDownPosition[0] = event.offsetX
       pointerDownPosition[1] = event.offsetY
       pointerDownScreenPosition[0] = event.screenX
@@ -99,7 +105,19 @@ export default class DamdomPadXYElement extends HTMLElement {
       case 'value':
         this.value = new Function(`return ${newValue}`).apply(this)
         break
+      case 'min':
+        this.#min = JSON.parse(newValue)
+        break
+      case 'max':
+        this.#max = JSON.parse(newValue)
+        break
     }
+  }
+
+  #updatePointer() {
+    const x = (this.#value[0] - this.#min[0]) / (this.#max[0] - this.#min[0])
+    const y = (this.#value[1] - this.#min[1]) / (this.#max[1] - this.#min[1])
+    this.#pointer.style.transform = `translate(${x * this.#width - this.#width * .5}px, ${-(y * this.#height - this.#height * .5)}px)`
   }
 
   get disabled() {
@@ -114,20 +132,32 @@ export default class DamdomPadXYElement extends HTMLElement {
     }
   }
 
-  _updatePointer() {
-    this._pointer.style.transform = `translate(${this._value[0] * this._width * .5}px, ${-this._value[1] * this._height * .5}px)`
+  get min() {
+    return this.getAttribute('min')
+  }
+
+  set min(value) {
+    this.setAttribute('min', JSON.stringify(value))
+  }
+
+  get max() {
+    return this.getAttribute('max')
+  }
+
+  set max(value) {
+    this.setAttribute('max', JSON.stringify(value))
   }
 
   get value() {
-    return this._value
+    return this.#value
   }
 
   set value(value) {
-    if (this._value[0] === value[0] && this._value[1] === value[1]) {
+    if (this.#value[0] === value[0] && this.#value[1] === value[1]) {
       return
     }
-    this._value = value
-    this._updatePointer()
+    this.#value = value
+    this.#updatePointer()
     this.dispatchEvent(new Event('input', {
       bubbles: true,
     }))
