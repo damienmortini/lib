81edd0bb9802ccd3bfe94f12fa99269ee0436ca8
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2022-02-19 14:48:29 +0100

    TrackballController: update

--- packages/library/core/input/TrackballController.js
+++ packages/library/core/input/TrackballController.js
@@ -5,12 +5,22 @@ import Quaternion from '@damienmortini/math/Quaternion.js'
 import GestureObserver from './GestureObserver.js'
 
 export default class TrackballController {
+  #cachedMatrix = new Matrix4()
+  #cachedQuaternion = new Quaternion()
+  #positionOffset = new Vector3()
+  #cachedVector3 = new Vector3()
+  #velocity = new Vector2()
+  #velocityOrigin = new Vector2()
+  #distanceEased
+  #position
+  #distance
+
   constructor({
     matrix = new Matrix4(),
     domElement = document.body,
-    invertRotation = true,
+    inverted = false,
     rotationVelocity = Math.PI / 180,
-    rotationEaseRatio = .1,
+    rotationEaseRatio = .05,
     distance = 0,
     distanceMin = 0,
     distanceMax = Infinity,
@@ -21,7 +31,7 @@ export default class TrackballController {
   } = {}) {
     this.matrix = matrix
 
-    this.invertRotation = invertRotation
+    this.inverted = inverted
     this.rotationVelocity = rotationVelocity
     this.rotationEaseRatio = rotationEaseRatio
     this.distanceMax = distanceMax
@@ -31,16 +41,7 @@ export default class TrackballController {
     this.zoomDisabled = zoomDisabled
     this.distance = distance
 
-    this._cachedQuaternion = new Quaternion()
-    this._cachedMatrix = new Matrix4()
-    this._cachedVector3 = new Vector3()
-
-    this._velocity = new Vector2()
-    this._velocityOrigin = new Vector2()
-
-    this._position = new Vector3([this.matrix.x, this.matrix.y, this.matrix.z])
-    this._positionPrevious = this._position.clone()
-    this._positionOffset = new Vector3()
+    this.#position = new Vector3([this.matrix.x, this.matrix.y, this.matrix.z])
 
     this.update()
 
@@ -48,65 +49,71 @@ export default class TrackballController {
 
     domElement.addEventListener('wheel', (event) => {
       if (this.zoomDisabled || this.disabled) return
-      this._distance *= 1 + event.deltaY * this.zoomVelocity * .01
-      this._distance = Math.max(this.distanceMin, Math.min(this.distanceMax, this._distance))
+      this.#distance *= 1 + event.deltaY * this.zoomVelocity * .01
+      this.#distance = Math.max(this.distanceMin, Math.min(this.distanceMax, this.#distance))
     }, { passive: true })
 
     const gestureObserver = new GestureObserver((gesture) => {
       if (this.disabled) {
         return
       }
-      this._velocity.x += (gesture.movementX * this.rotationVelocity - this._velocity.x) * rotationEaseRatio
-      this._velocity.y += (gesture.movementY * this.rotationVelocity - this._velocity.y) * rotationEaseRatio
+      this.#velocity.x += (gesture.movementX * this.rotationVelocity - this.#velocity.x) * rotationEaseRatio
+      this.#velocity.y += (gesture.movementY * this.rotationVelocity - this.#velocity.y) * rotationEaseRatio
       if (!this.zoomDisabled) {
-        this._distance *= 1 + (1 - gesture.movementScale) * this.zoomVelocity * 10
-        this._distance = Math.max(this.distanceMin, Math.min(this.distanceMax, this._distance))
+        this.#distance *= 1 + (1 - gesture.movementScale) * this.zoomVelocity * 10
+        this.#distance = Math.max(this.distanceMin, Math.min(this.distanceMax, this.#distance))
       }
     }, { pointerCapture: true })
     gestureObserver.observe(domElement)
   }
 
   set distance(value) {
-    this._distance = this._distanceEased = value
+    this.#distance = this.#distanceEased = value
   }
 
   get distance() {
-    return this._distance
+    return this.#distance
   }
 
   get distanceEased() {
-    return this._distanceEased
+    return this.#distanceEased
   }
 
   update() {
-    this._cachedMatrix.identity()
-    this._cachedQuaternion.identity()
+    this.#cachedMatrix.identity()
+    this.#cachedQuaternion.identity()
 
-    this._distanceEased += (this._distance - this._distanceEased) * this.zoomEaseRatio
+    this.#distanceEased += (this.#distance - this.#distanceEased) * this.zoomEaseRatio
 
-    this._position.set(this.matrix.x, this.matrix.y, this.matrix.z).subtract(this._positionOffset)
+    this.#position.set(this.matrix.x, this.matrix.y, this.matrix.z).subtract(this.#positionOffset)
 
     this.matrix.x = 0
     this.matrix.y = 0
     this.matrix.z = 0
 
-    this._velocity.lerp(this._velocityOrigin, this.rotationEaseRatio)
+    this.#velocity.lerp(this.#velocityOrigin, this.rotationEaseRatio)
 
-    this._cachedQuaternion.rotateY(this.invertRotation ? -this._velocity.x : this._velocity.x)
-    this._cachedQuaternion.rotateX(this.invertRotation ? -this._velocity.y : this._velocity.y)
+    this.#cachedQuaternion.rotateY(-this.#velocity.x)
+    this.#cachedQuaternion.rotateX(-this.#velocity.y)
 
-    this._cachedMatrix.fromQuaternion(this._cachedQuaternion)
+    this.#cachedMatrix.fromQuaternion(this.#cachedQuaternion)
 
-    this.matrix.multiply(this._cachedMatrix)
+    if (this.inverted) {
+      this.#cachedMatrix.invert()
+      this.#cachedMatrix.multiply(this.matrix)
+      this.matrix.copy(this.#cachedMatrix)
+    } else {
+      this.matrix.multiply(this.#cachedMatrix)
+    }
 
-    this._positionOffset.set(0, 0, 1)
-    this._positionOffset.applyMatrix4(this.matrix)
-    this._positionOffset.scale(this._distanceEased)
+    this.#positionOffset.set(0, 0, 1)
+    this.#positionOffset.applyMatrix4(this.matrix)
+    this.#positionOffset.scale(this.#distanceEased)
 
-    this._cachedVector3.copy(this._position).add(this._positionOffset)
+    this.#cachedVector3.copy(this.#position).add(this.#positionOffset)
 
-    this.matrix.x = this._cachedVector3.x
-    this.matrix.y = this._cachedVector3.y
-    this.matrix.z = this._cachedVector3.z
+    this.matrix.x = this.#cachedVector3.x
+    this.matrix.y = this.#cachedVector3.y
+    this.matrix.z = this.#cachedVector3.z
   }
 }
