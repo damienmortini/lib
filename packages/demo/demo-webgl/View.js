5b1adb9bf379eafc9414835c3427a0bcbe412b44
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2022-03-16 18:40:11 +0100

    demos -> demo

---
+++ packages/demo/demo-webgl/View.js
@@ -0,0 +1,80 @@
+import Camera from '@damienmortini/math/Camera.js'
+import OrbitController from '@damienmortini/core/input/OrbitController.js'
+import GLBoxObject from '@damienmortini/webgl/object/GLBoxObject.js'
+import GLProgram from '@damienmortini/webgl/GLProgram.js'
+import BasicShader from '@damienmortini/webgl/shader/BasicShader.js'
+import Matrix4 from '@damienmortini/math/Matrix4.js'
+
+export default class View {
+  #canvas
+  #camera
+  #controller
+  #gl
+  #object
+  #objectTransform = new Matrix4()
+
+  constructor({
+    canvas,
+  }) {
+    this.#canvas = canvas
+
+    this.#gl = this.#canvas.getContext('webgl2', {
+      depth: true,
+      alpha: false,
+      antialias: true,
+    })
+
+    this.#camera = new Camera()
+
+    this.#controller = new OrbitController({
+      domElement: this.#canvas,
+      matrix: this.#camera.transform,
+      distance: 5,
+    })
+
+    this.#gl.clearColor(0, 0, 0, 1)
+    this.#gl.enable(this.#gl.CULL_FACE)
+    this.#gl.enable(this.#gl.DEPTH_TEST)
+
+    this.#object = new GLBoxObject({
+      gl: this.#gl,
+      normals: true,
+      program: new GLProgram({
+        gl: this.#gl,
+        ...new BasicShader({
+          normals: true,
+          vertexChunks: [
+            ['end', `
+              gl_Position = projectionView * transform * vec4(position, 1.);
+            `],
+          ],
+          fragmentChunks: [
+            ['end', `
+              fragColor = vec4(vNormal * .5 + .5, 1.);
+            `],
+          ],
+        }),
+      }),
+    })
+  }
+
+  resize(width, height) {
+    this.#gl.viewport(0, 0, this.#gl.drawingBufferWidth, this.#gl.drawingBufferHeight)
+    this.#camera.aspectRatio = width / height
+    this.update()
+  }
+
+  update() {
+    this.#gl.clear(this.#gl.COLOR_BUFFER_BIT | this.#gl.DEPTH_BUFFER_BIT)
+
+    this.#controller.update()
+
+    this.#object.draw({
+      bind: true,
+      uniforms: {
+        transform: this.#objectTransform,
+        projectionView: this.#camera.projectionView,
+      },
+    })
+  }
+}
---
+++ packages/demo/demo-webgl/index.js
@@ -0,0 +1,56 @@
+import DamdomTickerElement from '@damienmortini/damdom-ticker/index.js'
+
+import View from './View.js'
+
+class DemoWebGLElement extends DamdomTickerElement {
+  #canvas
+  #view
+
+  constructor() {
+    super()
+
+    this.callback = this.#update
+
+    this.attachShadow({ mode: 'open' }).innerHTML = `
+      <style>
+        :host {
+          display: block;
+          position: relative;
+          contain: content;
+          width: 500px;
+          height: 500px;
+        }
+        
+        canvas {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+        }
+      </style>
+      <canvas></canvas>
+    `
+
+    this.#canvas = this.shadowRoot.querySelector('canvas')
+
+    this.#view = new View({ canvas: this.#canvas })
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      const width = entries[0].contentRect.width
+      const height = entries[0].contentRect.height
+
+      this.#canvas.width = width * devicePixelRatio
+      this.#canvas.height = height * devicePixelRatio
+
+      this.#view.resize(width, height)
+    })
+    resizeObserver.observe(this.#canvas)
+  }
+
+  #update = () => {
+    this.#view.update()
+  }
+}
+
+customElements.define('demo-webgl', DemoWebGLElement)
