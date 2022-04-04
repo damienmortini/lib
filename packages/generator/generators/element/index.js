351243b6ac859a99f4939d0c25ad217a65baaa3d
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2022-04-04 16:42:49 +0200

    update generator

--- packages/generator/generators/element/index.js
+++ packages/generator/generators/element/index.js
@@ -7,24 +7,36 @@ const __dirname = dirname(__filename)
 
 /**
  * @param {Object} object
- * @param {String} object.elementScope Element scope
+ * @param {String} [object.template=basic] Element template
+ * @param {String} [object.scope=$rootScope] Package scope
+ * @param {String} [object.elementScope=$rootName] Element scope
  * @param {String} object.elementName Element name
- * @param {String} [object.scope=damienmortini] Package scope
  * @param {String} [object.path=./packages] Output path
  */
-export default function ({ elementScope, elementName, scope = 'damienmortini', path = './packages' }) {
+export default function ({ template, elementScope, elementName, scope, path }) {
   const fullElementName = `${elementScope}-${elementName}`.replaceAll(' ', '').toLowerCase()
   const fullPath = `${path}/${fullElementName}`
+  fs.mkdirSync(fullPath)
+  const fileNames = fs.readdirSync(`${__dirname}/templates/${template}`)
+  let packageJSON = {}
+  for (const fileName of fileNames) {
+    if (fileName === 'package.json') {
+      packageJSON = fs.readJSONSync(`${__dirname}/templates/${template}/${fileName}`)
+    } else if (fileName === 'index.js') {
+      let indexFileContent = fs.readFileSync(`${__dirname}/templates/${template}/${fileName}`, { encoding: 'utf-8' })
+      indexFileContent = indexFileContent.replaceAll('template-element', fullElementName)
+      indexFileContent = indexFileContent.replaceAll('template title', elementName)
+      const elementClass = `${elementScope} ${elementName}`.replace(/(^| )(.)/g, (result) => result.replace(' ', '').toUpperCase()) + 'Element'
+      indexFileContent = indexFileContent.replaceAll('TemplateElement', elementClass)
+      fs.writeFileSync(`${fullPath}/${fileName}`, indexFileContent)
+    } else {
+      fs.copyFileSync(`${__dirname}/templates/${template}/${fileName}`, `${fullPath}/${fileName}`)
+    }
+  }
   fs.outputFileSync(`${fullPath}/package.json`, JSON.stringify({
     name: `@${scope}/${fullElementName}`,
     private: true,
     version: '0.0.0',
+    ...packageJSON,
   }, null, 2))
-  let indexFileContent = fs.readFileSync(`${__dirname}/template/index.js`, { encoding: 'utf-8' })
-  indexFileContent = indexFileContent.replaceAll('template-element', fullElementName)
-  indexFileContent = indexFileContent.replaceAll('template title', elementName)
-  const elementClass = `${elementScope} ${elementName}`.replace(/(^| )(.)/g, (result) => result.replace(' ', '').toUpperCase()) + 'Element'
-  indexFileContent = indexFileContent.replaceAll('TemplateElement', elementClass)
-  fs.writeFileSync(`${fullPath}/index.js`, indexFileContent)
-  fs.copyFileSync(`${__dirname}/template/index.css`, `${fullPath}/index.css`)
 }
