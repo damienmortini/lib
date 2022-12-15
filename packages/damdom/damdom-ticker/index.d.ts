dc7a63e043eecf89a3a146e5c3f785b410311bf8
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2022-12-15 08:14:58 +0100

    update types

--- packages/damdom/damdom-ticker/index.d.ts
+++ packages/damdom/damdom-ticker/index.d.ts
@@ -3,23 +3,23 @@
  * @hideconstructor
  */
 export class DamdomTickerElement extends HTMLElement {
-    static get deltaTime(): any;
-    connectedCallback(): void;
-    disconnectedCallback(): void;
-    /**
-     * Play element.
-     */
-    play(): void;
-    /**
-     * Pause element.
-     */
-    pause(): void;
-    /**
-     * Tells whether the element is paused.
-     * @type {Boolean}
-     * @readonly
-     */
-    readonly get paused(): boolean;
-    set callback(arg: any);
-    #private;
+  static get deltaTime(): number
+  connectedCallback(): void
+  disconnectedCallback(): void
+  /**
+   * Play element.
+   */
+  play(): void
+  /**
+   * Pause element.
+   */
+  pause(): void
+  /**
+   * Tells whether the element is paused.
+   * @type {Boolean}
+   * @readonly
+   */
+  get paused(): boolean
+  set callback(arg: any)
+  #private
 }
