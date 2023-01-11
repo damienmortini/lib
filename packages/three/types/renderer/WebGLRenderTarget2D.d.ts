8f6b7dd7005e9fc6c9777395d94fd161a2d9f419
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2023-01-11 14:00:43 +0100

    update three

---
+++ packages/three/types/renderer/WebGLRenderTarget2D.d.ts
@@ -0,0 +1,42 @@
+export class WebGLRenderTarget2D {
+  constructor({
+    renderer,
+    material,
+    width,
+    height,
+    wrapS,
+    wrapT,
+    magFilter,
+    minFilter,
+    format,
+    type,
+    anisotropy,
+    encoding,
+    generateMipmaps,
+    depthBuffer,
+    stencilBuffer,
+    depthTexture,
+  }: {
+    renderer: any
+    material?: any
+    width?: number
+    height?: number
+    wrapS?: any
+    wrapT?: any
+    magFilter?: any
+    minFilter?: any
+    format?: any
+    type?: any
+    anisotropy?: any
+    encoding?: any
+    generateMipmaps?: any
+    depthBuffer?: any
+    stencilBuffer?: any
+    depthTexture?: any
+  })
+  renderer: any
+  set material(arg: any)
+  get material(): any
+  render({ debug }?: { debug?: boolean }): void
+  #private
+}
