b73fecf8c2bcb8d3104da248d8cb9153448d3e0a
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2023-01-30 15:10:33 -0500

    clean

--- packages/webgl/--target=esnext/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from "./GLProgram.js";
-export * from "./GLGeometry.js";
-export * from "./GLTexture.js";
-export * from "./GLObject.js";
-export * from "./object/GLBoxObject.js";
-export * from "./object/GLPlaneObject.js";
-export * from "./shader/BasicShader.js";
