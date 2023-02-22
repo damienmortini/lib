eb9821c51aa1ebd4817cc4790786eac31a8bc6fd
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2023-02-22 17:41:46 -0500

    generator: css > style

--- packages/generator/generators/element/templates/basic/index.js
+++ packages/generator/generators/element/templates/basic/index.js
@@ -1,11 +1,11 @@
-import css from './index.css' assert { type: 'css' }
+import style from './index.css' assert { type: 'css' }
 
 export class TemplateElement extends HTMLElement {
   constructor() {
     super()
 
     this.attachShadow({ mode: 'open' })
-    this.shadowRoot.adoptedStyleSheets = [css]
+    this.shadowRoot.adoptedStyleSheets = [style]
     this.shadowRoot.innerHTML = `template title`
   }
 }
