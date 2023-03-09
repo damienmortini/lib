fd667dfa1ae6eaef2a63fd75da524463da059fb1
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2023-03-09 16:02:20 -0500

    dependencycleaner: first commit

---
+++ packages/dependencycleaner/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import depcheck from 'depcheck'
+import { execSync } from 'child_process'
+
+const results = await depcheck(process.cwd(), {})
+
+const unusedDependencies = results.dependencies
+if (unusedDependencies.length) {
+  console.log(`Removing unused dependencies: \n${unusedDependencies.join('\n')}`)
+  execSync(`npm uninstall ${unusedDependencies.join(' ')}`)
+}
+
+const missingDependencies = [...Object.keys(results.missing)]
+if (missingDependencies.length) {
+  console.log(`Installing missing dependencies: \n${missingDependencies.join('\n')}`)
+  execSync(`npm install ${missingDependencies.join(' ')}`)
+}
