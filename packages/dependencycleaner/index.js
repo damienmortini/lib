858aa35665cf6c66a397894fe1af1a2af0e91cb6
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2023-03-10 18:03:25 -0500

    depclean: better logs

--- packages/dependencycleaner/index.js
+++ packages/dependencycleaner/index.js
@@ -5,14 +5,16 @@ import { execSync } from 'child_process'
 
 const results = await depcheck(process.cwd(), {})
 
+console.log(`Running depclean on ${process.cwd()}`)
+
 const unusedDependencies = results.dependencies
 if (unusedDependencies.length) {
-  console.log(`Removing unused dependencies: \n${unusedDependencies.join('\n')}`)
+  console.log(`Removing unused dependencies: \n- ${unusedDependencies.join('\n- ')}`)
   execSync(`npm uninstall ${unusedDependencies.join(' ')}`)
 }
 
 const missingDependencies = [...Object.keys(results.missing)]
 if (missingDependencies.length) {
-  console.log(`Installing missing dependencies: \n${missingDependencies.join('\n')}`)
+  console.log(`Installing missing dependencies: \n- ${missingDependencies.join('\n- ')}`)
   execSync(`npm install ${missingDependencies.join(' ')}`)
 }
