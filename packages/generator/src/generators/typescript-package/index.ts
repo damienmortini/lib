6f9b9be5ab05af13b2e0f1e3dd7df746e53c3e1c
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2023-07-28 16:50:53 -0400

    update generator

--- packages/generator/src/generators/typescript-package/index.ts
+++ packages/generator/src/generators/typescript-package/index.ts
@@ -7,41 +7,39 @@ import { mkdir, writeFile } from 'fs/promises'
  * @param {String} [object.path=./packages] Output path
  */
 export default async function ({ name, scope, path }) {
-  /** Create path if it doesn't exists */
-  await mkdir(path, { recursive: true })
+  await mkdir(`${path}/${name}`, { recursive: true })
 
-  // await Promise.all([
-  //   writeFile(
-  //     `${path}/${name}/package.json`,
-  //     JSON.stringify(
-  //       {
-  //         name: `@${scope}/${name}`,
-  //         private: true,
-  //         version: '0.0.0',
-  //         type: 'module',
-  //         main: 'dist/index.js',
-  //         scripts: {
-  //           build: 'tsc',
-  //           watch: 'tsc -w',
-  //         },
-  //         devDependencies: {
-  //           typescript: '*',
-  //         },
-  //       },
-  //       null,
-  //       2,
-  //     ),
-  //   ),
-  //   writeFile(
-  //     `${path}/${name}/tsconfig.json`,
-  //     JSON.stringify({
-  //       extends: '../../tsconfig.json',
-  //       compilerOptions: {
-  //         outDir: './dist',
-  //       },
-  //     }),
-  //   ),
-  //   writeFile(`${path}/${name}/.gitignore`, 'node_modules\n/dist'),
-  //   writeFile(`${path}/${name}/src/index.ts`, ''),
-  // ])
+  await Promise.all([
+    writeFile(
+      `${path}/${name}/package.json`,
+      JSON.stringify(
+        {
+          name: `@${scope}/${name}`,
+          private: true,
+          version: '0.0.0',
+          type: 'module',
+          main: 'dist/index.js',
+          scripts: {
+            build: 'tsc',
+          },
+          devDependencies: {
+            typescript: '*',
+          },
+        },
+        null,
+        2,
+      ),
+    ),
+    writeFile(
+      `${path}/${name}/tsconfig.json`,
+      JSON.stringify({
+        extends: '../../tsconfig.json',
+        compilerOptions: {
+          outDir: './dist',
+        },
+      }),
+    ),
+    writeFile(`${path}/${name}/.gitignore`, 'node_modules\n/dist'),
+    writeFile(`${path}/${name}/src/index.ts`, ''),
+  ])
 }
