2f45fe226471be0ef4f18fe6dcaf48a2f756b066
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2023-07-28 19:31:18 -0400

    update generator

--- packages/generator/src/generators/element/index.ts
+++ packages/generator/src/generators/element/index.ts
@@ -18,13 +18,13 @@ export default function ({ template, elementScope, elementName, scope, path }) {
   const fullElementName = `${elementScope}-${flattenElementname}`.replaceAll(' ', '').toLowerCase()
   const fullPath = `${path}/${flattenElementname}element`
   fs.ensureDirSync(fullPath)
-  const fileNames = fs.readdirSync(`${__dirname}/templates/${template}`)
+  const fileNames = fs.readdirSync(`${__dirname}/../../templates/${template}`)
   let packageJSON = {}
   for (const fileName of fileNames) {
     if (fileName === '_package.json') {
-      packageJSON = fs.readJSONSync(`${__dirname}/templates/${template}/${fileName}`)
+      packageJSON = fs.readJSONSync(`${__dirname}/../../templates/${template}/${fileName}`)
     } else if (fileName === 'index.js') {
-      let indexFileContent = fs.readFileSync(`${__dirname}/templates/${template}/${fileName}`, { encoding: 'utf-8' })
+      let indexFileContent = fs.readFileSync(`${__dirname}/../../templates/${template}/${fileName}`, { encoding: 'utf-8' })
       indexFileContent = indexFileContent.replaceAll('template-element', fullElementName)
       indexFileContent = indexFileContent.replaceAll('template title', elementName)
       const elementClass = `${elementScope} ${elementName} Element`.replaceAll(/(\s+|^)(.)/g, (match, captureGroup1, captureGroup2) =>
@@ -33,7 +33,7 @@ export default function ({ template, elementScope, elementName, scope, path }) {
       indexFileContent = indexFileContent.replaceAll('TemplateElement', elementClass)
       fs.writeFileSync(`${fullPath}/${fileName}`, indexFileContent)
     } else {
-      fs.copyFileSync(`${__dirname}/templates/${template}/${fileName}`, `${fullPath}/${fileName}`)
+      fs.copyFileSync(`${__dirname}/../../templates/${template}/${fileName}`, `${fullPath}/${fileName}`)
     }
   }
   fs.outputFileSync(
