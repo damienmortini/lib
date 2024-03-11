3b00938334dec9db606dfa8de7e630ae2d6fc584
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2024-03-11 15:28:12 -0400

    clean

--- packages/server/bin/server-bin.js
+++ packages/server/bin/server-bin.js
@@ -1,27 +1,27 @@
 #!/usr/bin/env node
 
-import Server from '../Server.js'
+import { Server } from '../server.js';
 
 /**
  * Extract parameters
  */
-let verbose = false
-let resolveModules = false
-let path
-let rootPath
-let watchIgnore
+let verbose = false;
+let resolveModules = false;
+let path;
+let rootPath;
+let watchIgnore;
 
 for (const arg of process.argv) {
   if (arg.startsWith('--path')) {
-    path = arg.split('=')[1].trim()
+    path = arg.split('=')[1].trim();
   } else if (arg.startsWith('--root')) {
-    rootPath = arg.split('=')[1].trim()
+    rootPath = arg.split('=')[1].trim();
   } else if (arg.startsWith('--watchignore')) {
-    watchIgnore = arg.split('=')[1].trim()
+    watchIgnore = arg.split('=')[1].trim();
   } else if (arg === '--verbose') {
-    verbose = true
+    verbose = true;
   } else if (arg === '--resolve-modules') {
-    resolveModules = true
+    resolveModules = true;
   }
 }
 
@@ -32,4 +32,4 @@ new Server({
   watchIgnore,
   verbose,
   resolveModules,
-})
+});
