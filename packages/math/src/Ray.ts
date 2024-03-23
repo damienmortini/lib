5291e1cf37946a9a7f647ff67f1367eb2eb7cf76
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2024-03-23 16:15:50 -0400

    upgrade

--- packages/math/src/Ray.ts
+++ packages/math/src/Ray.ts
@@ -1,24 +1,24 @@
-import { Vector3 } from './Vector3.js'
-import { Matrix4 } from './Matrix4.js'
+import { Matrix4 } from './Matrix4.js';
+import { Vector3 } from './Vector3.js';
 
-const MATRIX4 = new Matrix4()
+const MATRIX4 = new Matrix4();
 
 export class Ray {
-  origin = new Vector3()
-  direction = new Vector3()
+  origin = new Vector3();
+  direction = new Vector3();
 
   setFromCamera(camera, position = [0, 0]) {
-    this.origin.x = camera.transform.x
-    this.origin.y = camera.transform.y
-    this.origin.z = camera.transform.z
+    this.origin.x = camera.transform.x;
+    this.origin.y = camera.transform.y;
+    this.origin.z = camera.transform.z;
 
-    this.direction.set([position[0], position[1], 1])
-    MATRIX4.invert(camera.projection)
-    this.direction.applyMatrix4(MATRIX4)
-    this.direction.applyMatrix4(camera.transform)
-    this.direction.subtract(this.origin)
-    this.direction.normalize()
+    this.direction.set([position[0], position[1], 1]);
+    MATRIX4.invert(camera.projection);
+    this.direction.applyMatrix4(MATRIX4);
+    this.direction.applyMatrix4(camera.transform);
+    this.direction.subtract(this.origin);
+    this.direction.normalize();
 
-    return this
+    return this;
   }
 }
