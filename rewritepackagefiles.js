6e90511d4ceeef24ce196b78b418c262dfda9937
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2024-03-29 13:09:48 -0400

    type module fixes

--- rewritepackagefiles.js
+++ rewritepackagefiles.js
@@ -1,13 +1,14 @@
-import fs from 'fs'
-import fastGlob from 'fast-glob'
+import fastGlob from 'fast-glob';
+import fs from 'fs';
 
 // Re-write package.json
-for (const result of fastGlob.sync(['*/package.json', 'element/*/package.json'], {
+for (const result of fastGlob.sync(['**/package.json'], {
   cwd: 'packages',
+  ignore: ['**/node_modules'],
   objectMode: true,
 })) {
-  const filePath = `packages/${result.path}`
-  const directory = filePath.replace(`/${result.name}`, '')
+  const filePath = `packages/${result.path}`;
+  const directory = filePath.replace(`/${result.name}`, '');
   const packageData = {
     ...JSON.parse(fs.readFileSync(filePath, 'utf-8')),
     author: 'Damien Mortini',
@@ -23,8 +24,9 @@ for (const result of fastGlob.sync(['*/package.json', 'element/*/package.json'],
     },
     bugs: 'https://github.com/damienmortini/lib/issues',
     homepage: `https://github.com/damienmortini/lib/tree/main/${directory}`,
-  }
-  fs.writeFileSync(filePath, `${JSON.stringify(packageData, null, 2)}\n`)
+  };
+  console.log(packageData.type);
+  fs.writeFileSync(filePath, `${JSON.stringify(packageData, null, 2)}\n`);
 }
 
 // Re-write elements README
@@ -32,8 +34,8 @@ for (const result of fastGlob.sync(['*/README.md'], {
   cwd: 'packages/element',
   objectMode: true,
 })) {
-  const elementName = `${result.path.replace(`/${result.name}`, '')}`
-  const filePath = `packages/element/${result.path}`
+  const elementName = `${result.path.replace(`/${result.name}`, '')}`;
+  const filePath = `packages/element/${result.path}`;
   fs.writeFileSync(
     filePath,
     `# \`<damo-${elementName}>\`
@@ -63,5 +65,5 @@ npm install @damienmortini/element-${elementName}
 
 <my-element-name></my-element-name>
 \`\`\``,
-  )
+  );
 }
