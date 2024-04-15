39cb5b180ef9677540265345c218dc9d486a366a
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2024-04-15 16:33:19 -0400

    small fixes

--- packages/element/starter-three/Scene.js
+++ packages/element/starter-three/Scene.js
@@ -1,36 +1,31 @@
-import {
-  Scene as THREEScene, PerspectiveCamera,
-  Mesh,
-  BoxGeometry,
-  MeshNormalMaterial,
-} from '../../three/src/Three.js'
+import TrackballController from '@damienmortini/core/input/TrackballController.js';
 
-import TrackballController from '@damienmortini/core/input/TrackballController.js'
+import { BoxGeometry, Mesh, MeshNormalMaterial, PerspectiveCamera, Scene as THREEScene } from '../../three/src/Three.js';
 
 export default class Scene extends THREEScene {
   constructor({ canvas }) {
-    super()
+    super();
 
-    this.camera = new PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10000)
+    this.camera = new PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10000);
 
     this.controls = new TrackballController({
       distance: 5,
       domElement: canvas,
-    })
-    this.camera.matrixAutoUpdate = false
+    });
+    this.camera.matrixAutoUpdate = false;
 
-    const cube = new Mesh(new BoxGeometry(1, 1, 1), new MeshNormalMaterial())
-    this.add(cube)
+    const cube = new Mesh(new BoxGeometry(1, 1, 1), new MeshNormalMaterial());
+    this.add(cube);
   }
 
   resize(width, height) {
-    this.camera.aspect = width / height
-    this.camera.updateProjectionMatrix()
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
   }
 
   update() {
-    this.controls.update()
-    this.camera.matrix.fromArray(this.controls.matrix)
-    this.camera.matrixWorldNeedsUpdate = true
+    this.controls.update();
+    this.camera.matrix.fromArray(this.controls.matrix);
+    this.camera.matrixWorldNeedsUpdate = true;
   }
 }
