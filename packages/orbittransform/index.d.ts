d6d302f52e0afa0b01fa1ea8566a00ca6699e81c
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2024-05-25 18:05:36 +0200

    OrbitTransform: add global disabled

--- packages/orbittransform/index.d.ts
+++ packages/orbittransform/index.d.ts
@@ -1,70 +1,53 @@
 export class OrbitTransform {
-  constructor({
-    matrix,
-    domElement,
-    pan,
-    tilt,
-    inverted,
-    distance,
-    distanceMin,
-    distanceMax,
-    tiltMin,
-    tiltMax,
-    tiltDisabled,
-    panMin,
-    panMax,
-    panDisabled,
-    rotationEasing,
-    rotationVelocity,
-    zoomEasing,
-    zoomVelocity,
-    zoomDisabled,
-  }: {
-    matrix?: Matrix4
-    domElement?: Window | HTMLElement
-    pan?: number
-    tilt?: number
-    inverted?: boolean
-    distance?: number
-    distanceMin?: number
-    distanceMax?: number
-    tiltMin?: number
-    tiltMax?: number
-    tiltDisabled?: boolean
-    panMin?: number
-    panMax?: number
-    panDisabled?: boolean
-    rotationEasing?: number
-    rotationVelocity?: number
-    zoomEasing?: number
-    zoomVelocity?: number
-    zoomDisabled?: boolean
-  })
-  matrix: Matrix4
-  inverted: boolean
-  distanceMax: number
-  distanceMin: number
-  zoomEasing: number
-  tiltMax: number
-  tiltMin: number
-  tiltDisabled: boolean
-  panMax: number
-  panMin: number
-  panDisabled: boolean
-  rotationEasing: number
-  rotationVelocity: number
-  zoomDisabled: boolean
-  zoomVelocity: number
-  panEnd: number
-  tiltEnd: number
-  distanceEnd: number
-  set pan(arg: number)
-  get pan(): number
-  set tilt(arg: number)
-  get tilt(): number
-  set distance(arg: number)
-  get distance(): number
-  update(): void
-  #private
+    constructor({ matrix, domElement, pan, tilt, inverted, disabled, distance, distanceMin, distanceMax, tiltMin, tiltMax, tiltDisabled, panMin, panMax, panDisabled, rotationEasing, rotationVelocity, zoomEasing, zoomVelocity, zoomDisabled, pointerCapture, }: {
+        matrix?: Matrix4;
+        domElement?: any;
+        pan?: number;
+        tilt?: number;
+        inverted?: boolean;
+        disabled?: boolean;
+        distance?: number;
+        distanceMin?: number;
+        distanceMax?: number;
+        tiltMin?: number;
+        tiltMax?: number;
+        tiltDisabled?: boolean;
+        panMin?: number;
+        panMax?: number;
+        panDisabled?: boolean;
+        rotationEasing?: number;
+        rotationVelocity?: number;
+        zoomEasing?: number;
+        zoomVelocity?: number;
+        zoomDisabled?: boolean;
+        pointerCapture?: boolean;
+    });
+    matrix: Matrix4;
+    inverted: boolean;
+    disabled: boolean;
+    distanceMax: number;
+    distanceMin: number;
+    zoomEasing: number;
+    tiltMax: number;
+    tiltMin: number;
+    tiltDisabled: boolean;
+    panMax: number;
+    panMin: number;
+    panDisabled: boolean;
+    rotationEasing: number;
+    rotationVelocity: number;
+    zoomDisabled: boolean;
+    zoomVelocity: number;
+    panEnd: number;
+    tiltEnd: number;
+    distanceEnd: number;
+    set pan(value: number);
+    get pan(): number;
+    set tilt(value: number);
+    get tilt(): number;
+    set distance(value: number);
+    get distance(): number;
+    update(): void;
+    #private;
 }
-import { Matrix4 } from '@damienmortini/math'
+import { Matrix4 } from '@damienmortini/math';
