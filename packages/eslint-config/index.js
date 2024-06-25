017d6ce165aeb070d092c578006952bd6adc4ce6
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2024-06-25 11:31:39 +0100

    start updating eslint

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,25 +1,31 @@
-module.exports = {
-  env: {
-    browser: true,
-    node: true,
-    es2021: true,
+import { configs as eslintRecommended } from "@eslint/js";
+import typescriptParser from "@typescript-eslint/parser";
+import { rules as typescriptRules, configs as typescriptConfigs } from "@typescript-eslint/eslint-plugin";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
+export default [
+  eslintRecommended.recommended,
+  typescriptConfigs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: typescriptParser,
+      globals: {
+        navigation: "readonly",
+        NavigateEvent: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescriptRules,
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "n/no-callback-literal": "off",
+      "no-empty": "warn",
+      "prefer-const": ["error", { destructuring: "all" }],
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['simple-import-sort', '@typescript-eslint'],
-  rules: {
-    'n/no-callback-literal': 'off',
-    'no-empty': 'warn',
-    'prefer-const': ['error', { destructuring: 'all' }],
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
-  },
-  globals: {
-    navigation: 'readonly',
-    NavigateEvent: 'readonly',
-  },
-};
+];
