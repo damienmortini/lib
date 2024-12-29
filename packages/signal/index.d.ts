987c266c79c7359c5eb81593ea4ea4c37efdde17
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2024-12-29 11:01:48 +0100

    convert signal to typescript

--- packages/signal/index.d.ts
+++
@@ -1,5 +0,0 @@
-export class Signal extends Set<any> {
-  constructor(values?: readonly any[]);
-  constructor(iterable?: Iterable<any>);
-  dispatch(value: any): void;
-}
--- packages/signal/index.js
+++
@@ -1,7 +0,0 @@
-export class Signal extends Set {
-  dispatch(value) {
-    for (const callback of this) {
-      callback(value);
-    }
-  }
-}
