8abcee727e0a447065b73d86729333c3d0a45343
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2024-12-29 11:30:41 +0100

    signal: fix type

--- packages/signal/src/index.ts
+++ packages/signal/src/index.ts
@@ -1,15 +1,15 @@
-export class Signal<T> extends Set<(value: T) => void> {
-  dispatch(value: T) {
+export class Signal<T = void> extends Set<(value?: T) => void> {
+  dispatch(value?: T) {
     for (const callback of this) {
       callback(value);
     }
   }
 
-  add(callback: (value: T) => void, options?: { once?: boolean }): this {
+  add(callback: (value?: T) => void, options?: { once?: boolean }): this {
     if (!options?.once) {
       return super.add(callback);
     }
-    const onceCallback = (value: T) => {
+    const onceCallback = (value?: T) => {
       callback(value);
       this.delete(onceCallback);
     };
