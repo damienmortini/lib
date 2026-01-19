b0766bc6140004cbc709dea6bcd382a8f1ab2ed7
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2026-01-19 10:50:30 -0500

    clean server bin

--- packages/server/src/bin/index.ts
+++ packages/server/src/bin/index.ts
@@ -1,13 +1,75 @@
 #!/usr/bin/env node
 
-import { spawn } from 'child_process';
+import { Server } from "../server.js";
 
-const serverBinPath = process.argv[1].replace('index.js', 'server-bin.js');
+const args = process.argv.slice(2);
+let verbose = false;
+let resolveModules = false;
+let path: string | undefined;
+let rootPath: string | undefined;
+let watchIgnore: Array<string | RegExp> | undefined;
+const watchPaths: Array<string> = [];
+let port: number | undefined;
+let useExternalCertificate = false;
+const proxy: { [path: string]: string } = {};
 
-spawn('node', [
-  '--preserve-symlinks',
-  '--preserve-symlinks-main',
-  '--experimental-import-meta-resolve',
-  serverBinPath,
-  ...process.argv.slice(2),
-], { stdio: 'inherit' });
+let i = 0;
+while (i < args.length) {
+  const arg = args[i];
+
+  switch (arg) {
+    case "--path":
+      path = args[++i];
+      break;
+    case "--root":
+      rootPath = args[++i];
+      break;
+    case "--watch-ignore":
+      watchIgnore = args[++i]?.split(",");
+      break;
+    case "--verbose":
+      verbose = true;
+      break;
+    case "--resolve-modules":
+      resolveModules = true;
+      break;
+    case "--port":
+      port = parseInt(args[++i]);
+      break;
+    case "--external-certificate":
+      useExternalCertificate = true;
+      break;
+    case "--proxy": {
+      const proxyPath = args[++i];
+      const proxyTarget = args[++i];
+      if (proxyPath && proxyTarget) {
+        proxy[proxyPath] = proxyTarget;
+      }
+      break;
+    }
+    case "--watch": {
+      const watchPath = args[++i];
+      if (watchPath) {
+        watchPaths.push(watchPath);
+      }
+      break;
+    }
+  }
+
+  i++;
+}
+
+const server = new Server({
+  path,
+  rootPath,
+  watch: true,
+  watchIgnore,
+  watchPaths,
+  verbose,
+  resolveModules,
+  port,
+  useExternalCertificate,
+  proxy,
+});
+
+await server.ready;
